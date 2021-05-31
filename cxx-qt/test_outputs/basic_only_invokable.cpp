b86da0f3e6a663e60d30e356bec475422eb1ccee
Andrew Hayzen <andrew.hayzen@kdab.com>
2021-05-31 18:44:38 +0100

    cpp_gen: generate a C++ header and source for an invokable QObject
    Change-Id: Ice3ff9e2ea4812cbf337a3cdfd663ade879a652f
    Reviewed-on: https://codereview.kdab.com/c/kdab/rust/cxx-qt/+/97956
    Reviewed-by: Gerhard de Clercq <gerhard.declercq@kdab.com>

---
+++ cxx-qt/test_outputs/basic_only_invokable.cpp
@@ -0,0 +1,21 @@
+#include "cxx-qt-gen/include/my_object.h"
+#include "cxx-qt-gen/src/my_object.rs.h"
+
+MyObject::MyObject(QObject *parent)
+    : QObject(parent)
+    , m_rustObj(create_my_object_rs())
+{
+}
+
+MyObject::~MyObject() = default;
+
+void MyObject::say_hi(const QString& string, int number) const
+{
+    auto rustString = rust::string(string.toUtf8().data(), bytes.length());
+    m_rustObj->say_hi(std::move(rustString), number);
+}
+
+std::unique_ptr<MyObject> new_MyObject()
+{
+    return std::make_unique<MyObject>();
+}
---
+++ cxx-qt/test_outputs/basic_only_invokable.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <QObject>
+#include <QString>
+
+#include "rust/cxx.h"
+
+class MyObjectRs;
+
+class MyObject : public QObject {
+    Q_OBJECT
+
+public:
+    MyObject(QObject *parent = nullptr);
+    ~MyObject();
+
+    Q_INVOKABLE void say_hi(const QString& string, int number) const;
+
+private:
+    rust::Box<MyObjectRs> m_rustObj;
+};
+
+std::unique_ptr<MyObject> new_MyObject();
