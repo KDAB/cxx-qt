45f1e56955710e95ef9467653ec8b582f4f81216
Andrew Hayzen <andrew.hayzen@kdab.com>
2021-12-09 18:52:33 +0000

    examples: add demo threading showing average energy usage
    Change-Id: I70ae7d9e37a289f0eb1533893d137508efacbbca

---
+++ examples/demo_threading/build.rs
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2021 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+use clang_format::ClangFormatStyle;
+use cxx_qt_build::CxxQtBuilder;
+
+fn main() {
+    CxxQtBuilder::new()
+        .cpp_format(ClangFormatStyle::Mozilla)
+        .file("src/lib.rs")
+        .build();
+}
