b83d8adc4fa191a58b0d4093790ca103a4a616a5
Andrew Hayzen <andrew.hayzen@kdab.com>
2022-01-12 11:41:21 +0000

    cxx_qt: fix clang-format issue
    Change-Id: I6c785b7934eb59eb099242de0a0aaa53a4a75e97

--- cxx-qt-gen/src/cxx_qt.cpp
+++ cxx-qt-gen/src/cxx_qt.cpp
@@ -363,7 +363,8 @@ extern "C"
 
   QVariantType cxxqt1$qvariant$get$type(const QVariant& self) noexcept
   {
-    // QVariant::Type is obsolete, ensure we use QMetaType::Type to avoid warnings
+    // QVariant::Type is obsolete, ensure we use QMetaType::Type to avoid
+    // warnings
     switch (static_cast<QMetaType::Type>(self.type())) {
       case QMetaType::Bool:
         return QVariantType::Bool;
