dd49365b97458328eedfdf9f2eaa335cbb8802bd
Andrew Hayzen <andrew.hayzen@kdab.com>
2022-02-28 14:25:36 +0000

    demo_threading: use Data struct to transfer data to Qt thread
    Change-Id: Ia5f795dcabd0782516545847532ef2733bf21272

--- examples/demo_threading/src/lib.rs
+++ examples/demo_threading/src/lib.rs
@@ -65,11 +65,7 @@ mod energy_usage {
         prelude::*,
         task::spawn,
     };
-    use futures::{
-        channel::mpsc::{channel, Receiver, Sender},
-        executor::block_on,
-        stream::StreamExt,
-    };
+    use futures::{executor::block_on, stream::StreamExt};
     use futures_timer::Delay;
     use std::{
         collections::HashMap,
@@ -78,12 +74,6 @@ mod energy_usage {
         time::{Duration, SystemTime},
     };
     use uuid::Uuid;
-
-    enum QtValueArrived {
-        AverageUse(f64),
-        Sensors(u32),
-        TotalUsage(f64),
-    }
 
     pub struct Data {
         average_use: f64,
@@ -102,8 +92,7 @@ mod energy_usage {
     }
 
     struct RustObj {
-        qt_sender: Sender<QtValueArrived>,
-        qt_receiver: Receiver<QtValueArrived>,
+        qt_values: Arc<Mutex<Option<Data>>>,
         join_handle_network: Option<JoinHandle<()>>,
         join_handle_timeout: Option<JoinHandle<()>>,
         join_handle_update: Option<JoinHandle<()>>,
@@ -111,11 +100,8 @@ mod energy_usage {
 
     impl Default for RustObj {
         fn default() -> Self {
-            let (qt_sender, qt_receiver) = channel(4096);
-
             Self {
-                qt_sender,
-                qt_receiver,
+                qt_values: Arc::new(Mutex::new(None)),
                 join_handle_network: None,
                 join_handle_timeout: None,
                 join_handle_update: None,
@@ -288,10 +274,10 @@ mod energy_usage {
                 }
             };
 
-            let mut qt_sender = self.qt_sender.clone();
             let sensors_mutex_update = sensors_mutex.clone();
             let update_requester = cpp.update_requester();
             let update_pair_update = update_pair.clone();
+            let qt_values_mutex = self.qt_values.clone();
             let run_update = async move {
                 loop {
                     let (lock, cvar) = &*update_pair_update;
@@ -301,21 +287,21 @@ mod energy_usage {
                         if *changed {
                             if let Ok(sensors) = sensors_mutex_update.lock() {
                                 // If there is new sensor info then build average, count, total and inform Qt
-                                let total = sensors.values().fold(0.0, |acc, x| acc + x.power);
-                                let count = sensors.len() as u32;
-                                let average = if count > 0 {
-                                    total / (count as f64)
+                                let total_use = sensors.values().fold(0.0, |acc, x| acc + x.power);
+                                let sensors = sensors.len() as u32;
+                                let average_use = if sensors > 0 {
+                                    total_use / (sensors as f64)
                                 } else {
                                     0.0
                                 };
 
-                                qt_sender
-                                    .try_send(QtValueArrived::TotalUsage(total))
-                                    .unwrap();
-                                qt_sender.try_send(QtValueArrived::Sensors(count)).unwrap();
-                                qt_sender
-                                    .try_send(QtValueArrived::AverageUse(average))
-                                    .unwrap();
+                                if let Ok(mut values) = qt_values_mutex.lock() {
+                                    *values = Some(Data {
+                                        average_use,
+                                        sensors,
+                                        total_use,
+                                    })
+                                }
 
                                 update_requester.request_update();
                             }
@@ -355,14 +341,12 @@ mod energy_usage {
 
     impl UpdateRequestHandler<CppObj<'_>> for RustObj {
         fn handle_update_request(&mut self, cpp: &mut CppObj) {
-            // Process each of the update requests from the background thread
-            while let Ok(event) = self.qt_receiver.try_next() {
-                if let Some(event) = event {
-                    match event {
-                        QtValueArrived::AverageUse(average) => cpp.set_average_use(average),
-                        QtValueArrived::Sensors(sensors) => cpp.set_sensors(sensors),
-                        QtValueArrived::TotalUsage(total) => cpp.set_total_use(total),
-                    }
+            // Process the new data from the background thread
+            if let Ok(mut values) = self.qt_values.lock() {
+                if let Some(data) = &*values {
+                    cpp.grab_values_from_data(data);
+
+                    *values = None;
                 }
             }
         }
