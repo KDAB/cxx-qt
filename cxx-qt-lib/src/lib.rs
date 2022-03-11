dc2c018bed27df7bb4fc296632f2ff83f849bd29
Andrew Hayzen <andrew.hayzen@kdab.com>
2022-03-11 18:07:16 +0000

    cxx-qt-lib: support Windows and Linux path separators
    Change-Id: Ia52e02c3ae53919b02ba2d8a6c0d7e2c8b5ec243

--- cxx-qt-lib/src/lib.rs
+++ cxx-qt-lib/src/lib.rs
@@ -49,71 +49,262 @@ pub use qvariant::{QVariant, Variant, VariantValue};
 mod map_qt_value;
 pub use map_qt_value::*;
 
-pub const QT_TYPES_HEADER: &str = include_str!("../include/qt_types.h");
+// Provide a separate depending on the platform
+// this is because include_str requires th correct and non-mixed path separators
+//
+// https://github.com/rust-lang/rust/issues/75075
+#[cfg(not(windows))]
+macro_rules! sep {
+    () => {
+        "/"
+    };
+}
+
+#[cfg(windows)]
+macro_rules! sep {
+    () => {
+        "\\"
+    };
+}
+
+pub const QT_TYPES_HEADER: &str =
+    include_str!(concat!("..", sep!(), "include", sep!(), "qt_types.h"));
 pub const QT_TYPES_SOURCE: &str = include_str!("qt_types.cpp");
 
-pub const QCOLOR_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qcolor_cxx.h"));
-pub const QCOLOR_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qcolor_cxx.cpp"));
-pub const QDATE_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qdate_cxx.h"));
-pub const QDATE_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qdate_cxx.cpp"));
+pub const QCOLOR_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qcolor_cxx.h"
+));
+pub const QCOLOR_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qcolor_cxx.cpp"
+));
+pub const QDATE_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qdate_cxx.h"
+));
+pub const QDATE_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qdate_cxx.cpp"
+));
 pub const QDATETIME_CXX_HEADER: &str = include_str!(concat!(
     env!("OUT_DIR"),
-    "/cxx-qt-lib/include/qdatetime_cxx.h"
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qdatetime_cxx.h"
 ));
 pub const QDATETIME_CXX_SOURCE: &str = include_str!(concat!(
     env!("OUT_DIR"),
-    "/cxx-qt-lib/src/qdatetime_cxx.cpp"
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qdatetime_cxx.cpp"
 ));
-pub const QPOINT_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qpoint_cxx.h"));
-pub const QPOINT_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qpoint_cxx.cpp"));
+pub const QPOINT_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qpoint_cxx.h"
+));
+pub const QPOINT_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qpoint_cxx.cpp"
+));
 pub const QPOINTF_CXX_HEADER: &str = include_str!(concat!(
     env!("OUT_DIR"),
-    "/cxx-qt-lib/include/qpointf_cxx.h"
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qpointf_cxx.h"
 ));
-pub const QPOINTF_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qpointf_cxx.cpp"));
-pub const QRECT_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qrect_cxx.h"));
-pub const QRECT_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qrect_cxx.cpp"));
-pub const QRECTF_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qrectf_cxx.h"));
-pub const QRECTF_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qrectf_cxx.cpp"));
-pub const QSIZE_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qsize_cxx.h"));
-pub const QSIZE_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qsize_cxx.cpp"));
-pub const QSIZEF_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qsizef_cxx.h"));
-pub const QSIZEF_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qsizef_cxx.cpp"));
+pub const QPOINTF_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qpointf_cxx.cpp"
+));
+pub const QRECT_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qrect_cxx.h"
+));
+pub const QRECT_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qrect_cxx.cpp"
+));
+pub const QRECTF_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qrectf_cxx.h"
+));
+pub const QRECTF_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qrectf_cxx.cpp"
+));
+pub const QSIZE_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qsize_cxx.h"
+));
+pub const QSIZE_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qsize_cxx.cpp"
+));
+pub const QSIZEF_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qsizef_cxx.h"
+));
+pub const QSIZEF_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qsizef_cxx.cpp"
+));
 pub const QSTRING_CXX_HEADER: &str = include_str!(concat!(
     env!("OUT_DIR"),
-    "/cxx-qt-lib/include/qstring_cxx.h"
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qstring_cxx.h"
 ));
-pub const QSTRING_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qstring_cxx.cpp"));
-pub const QTIME_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qtime_cxx.h"));
-pub const QTIME_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qtime_cxx.cpp"));
-pub const QURL_CXX_HEADER: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/include/qurl_cxx.h"));
-pub const QURL_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qurl_cxx.cpp"));
+pub const QSTRING_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qstring_cxx.cpp"
+));
+pub const QTIME_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qtime_cxx.h"
+));
+pub const QTIME_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qtime_cxx.cpp"
+));
+pub const QURL_CXX_HEADER: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qurl_cxx.h"
+));
+pub const QURL_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qurl_cxx.cpp"
+));
 pub const QVARIANT_CXX_HEADER: &str = include_str!(concat!(
     env!("OUT_DIR"),
-    "/cxx-qt-lib/include/qvariant_cxx.h"
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "include",
+    sep!(),
+    "qvariant_cxx.h"
 ));
-pub const QVARIANT_CXX_SOURCE: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/cxx-qt-lib/src/qvariant_cxx.cpp"));
+pub const QVARIANT_CXX_SOURCE: &str = include_str!(concat!(
+    env!("OUT_DIR"),
+    sep!(),
+    "cxx-qt-lib",
+    sep!(),
+    "src",
+    sep!(),
+    "qvariant_cxx.cpp"
+));
 
 pub trait PropertyChangeHandler<C, P> {
     fn handle_property_change(&mut self, cpp: &mut C, property: P);
