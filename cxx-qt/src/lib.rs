52251cda407de0b2235ce3ead10386254cc2beac
jason-yau <91023929+jason-yau@users.noreply.github.com>
2022-06-27 00:30:48 +0800

    Update lib.rs

--- cxx-qt/src/lib.rs
+++ cxx-qt/src/lib.rs
@@ -11,8 +11,8 @@ use cxx_qt_gen::{extract_qobject, generate_qobject_rs};
 /// Read the C++ namespace prefix that cxx-qt-build has set for us
 fn read_cpp_namespace_prefix() -> Vec<String> {
     let dir_target = std::env::var("OUT_DIR")
-        .and_then(|s| Ok(s + "/../../../.."))
-        .or(std::env::var("CARGO_MANIFEST_DIR").and_then(|s| Ok(s + "/target")))
+        .map(|s| s + "/../../../..")
+        .or(std::env::var("CARGO_MANIFEST_DIR").map(|s| s + "/target"))
         .expect("Could not get target dir");
     let path = format!("{}/cxx-qt-gen/cpp_namespace_prefix.txt", dir_target);
     let contents = std::fs::read_to_string(path).expect("Could not read cpp namespace prefix");
