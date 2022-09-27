d1eeb1fc339bae05deaadb14cfa1fb06de17f585
Andrew Hayzen <andrew.hayzen@kdab.com>
2022-09-27 10:26:28 +0100

    cxx-qt-gen: use a helper for the generation tests

--- crates/cxx-qt-gen/src/lib.rs
+++ crates/cxx-qt-gen/src/lib.rs
@@ -71,224 +71,124 @@ mod tests {
         syn::parse2(tokens.into_token_stream()).unwrap()
     }
 
+    /// Helper for testing if a given input Rust file generates an expected C++ header and source
+    macro_rules! test_cpp_generation {
+        ( $file_stem:literal ) => {
+            let parser = Parser::from(
+                syn::parse_str(include_str!(concat!("../test_inputs/", $file_stem, ".rs")))
+                    .unwrap(),
+            )
+            .unwrap();
+            let generated = GeneratedCppBlocks::from(&parser).unwrap();
+            let cpp = write_cpp(&generated);
+
+            let expected_header =
+                clang_format(include_str!(concat!("../test_outputs/", $file_stem, ".h"))).unwrap();
+            let expected_source = clang_format(include_str!(concat!(
+                "../test_outputs/",
+                $file_stem,
+                ".cpp"
+            )))
+            .unwrap();
+            assert_str_eq!(cpp.header, expected_header);
+            assert_str_eq!(cpp.source, expected_source);
+        };
+    }
+
+    /// Helper for testing if a given input Rust file generates an expected Rust source
+    macro_rules! test_rust_generation {
+        ( $file_stem:literal ) => {
+            let parser = Parser::from(
+                syn::parse_str(include_str!(concat!("../test_inputs/", $file_stem, ".rs")))
+                    .unwrap(),
+            )
+            .unwrap();
+            let generated = GeneratedRustBlocks::from(&parser).unwrap();
+            let rust = format_rs_source(&write_rust(&generated).to_string());
+
+            let expected_output =
+                format_rs_source(include_str!(concat!("../test_outputs/", $file_stem, ".rs")));
+            assert_str_eq!(rust, expected_output);
+        };
+    }
+
     #[test]
     fn generates_invokables_cpp() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/invokables.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header = clang_format(include_str!("../test_outputs/invokables.h")).unwrap();
-        let expected_source = clang_format(include_str!("../test_outputs/invokables.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("invokables");
     }
 
     #[test]
     fn generates_naming_cpp() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/naming.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header = clang_format(include_str!("../test_outputs/naming.h")).unwrap();
-        let expected_source = clang_format(include_str!("../test_outputs/naming.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("naming");
     }
 
     #[test]
     fn generates_properties_cpp() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/properties.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header = clang_format(include_str!("../test_outputs/properties.h")).unwrap();
-        let expected_source = clang_format(include_str!("../test_outputs/properties.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("properties");
     }
 
     #[test]
     fn generates_signals_cpp() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/signals.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header = clang_format(include_str!("../test_outputs/signals.h")).unwrap();
-        let expected_source = clang_format(include_str!("../test_outputs/signals.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("signals");
     }
 
     #[test]
     fn generates_types_primitive_property_cpp() {
-        let parser = Parser::from(
-            syn::parse_str(include_str!("../test_inputs/types_primitive_property.rs")).unwrap(),
-        )
-        .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header =
-            clang_format(include_str!("../test_outputs/types_primitive_property.h")).unwrap();
-        let expected_source =
-            clang_format(include_str!("../test_outputs/types_primitive_property.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("types_primitive_property");
     }
 
     #[test]
     fn generates_types_qt_property_cpp() {
-        let parser = Parser::from(
-            syn::parse_str(include_str!("../test_inputs/types_qt_property.rs")).unwrap(),
-        )
-        .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header =
-            clang_format(include_str!("../test_outputs/types_qt_property.h")).unwrap();
-        let expected_source =
-            clang_format(include_str!("../test_outputs/types_qt_property.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("types_qt_property");
     }
 
     #[test]
     fn generates_types_qt_invokable_cpp() {
-        let parser = Parser::from(
-            syn::parse_str(include_str!("../test_inputs/types_qt_invokable.rs")).unwrap(),
-        )
-        .unwrap();
-        let generated = GeneratedCppBlocks::from(&parser).unwrap();
-        let cpp = write_cpp(&generated);
-
-        let expected_header =
-            clang_format(include_str!("../test_outputs/types_qt_invokable.h")).unwrap();
-        let expected_source =
-            clang_format(include_str!("../test_outputs/types_qt_invokable.cpp")).unwrap();
-        assert_str_eq!(cpp.header, expected_header);
-        assert_str_eq!(cpp.source, expected_source);
+        test_cpp_generation!("types_qt_invokable");
     }
 
     #[test]
     fn generates_custom_default_rust() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/custom_default.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output = format_rs_source(include_str!("../test_outputs/custom_default.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("custom_default");
     }
 
     #[test]
     fn generates_invokables_rust() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/invokables.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output = format_rs_source(include_str!("../test_outputs/invokables.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("invokables");
     }
 
     #[test]
     fn generates_naming_rust() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/naming.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output = format_rs_source(include_str!("../test_outputs/naming.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("naming");
     }
 
     #[test]
     fn generates_passthrough_rust() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/passthrough.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output = format_rs_source(include_str!("../test_outputs/passthrough.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("passthrough");
     }
 
     #[test]
     fn generates_properties_rust() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/properties.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output = format_rs_source(include_str!("../test_outputs/properties.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("properties");
     }
 
     #[test]
     fn generates_signals_rust() {
-        let parser =
-            Parser::from(syn::parse_str(include_str!("../test_inputs/signals.rs")).unwrap())
-                .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output = format_rs_source(include_str!("../test_outputs/signals.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("signals");
     }
 
     #[test]
     fn generates_types_primitive_property_rust() {
-        let parser = Parser::from(
-            syn::parse_str(include_str!("../test_inputs/types_primitive_property.rs")).unwrap(),
-        )
-        .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output =
-            format_rs_source(include_str!("../test_outputs/types_primitive_property.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("types_primitive_property");
     }
 
     #[test]
     fn generates_types_qt_property_rust() {
-        let parser = Parser::from(
-            syn::parse_str(include_str!("../test_inputs/types_qt_property.rs")).unwrap(),
-        )
-        .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output =
-            format_rs_source(include_str!("../test_outputs/types_qt_property.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("types_qt_property");
     }
 
     #[test]
     fn generates_types_qt_invokable_rust() {
-        let parser = Parser::from(
-            syn::parse_str(include_str!("../test_inputs/types_qt_invokable.rs")).unwrap(),
-        )
-        .unwrap();
-        let generated = GeneratedRustBlocks::from(&parser).unwrap();
-        let rust = format_rs_source(&write_rust(&generated).to_string());
-
-        let expected_output =
-            format_rs_source(include_str!("../test_outputs/types_qt_invokable.rs"));
-        assert_str_eq!(rust, expected_output);
+        test_rust_generation!("types_qt_invokable");
     }
 }
