125fb41894e5f2be2bf11bb8d33c72e2fc3040a0
Magnus Groß <magnus.gross@kdab.com>
2023-01-11 00:57:23 +0100

    Add a template method to convert arbitrary Qt types to QString
    This uses the commonly available QDataStream& operator<< that is
    originally intended to print Qt types over the qDebug() interface.
    
    However in this case we use it to implement a generic toQString() method
    that works with pretty much all basic Qt types.

--- crates/cxx-qt-lib-headers/include/common.h
+++ crates/cxx-qt-lib-headers/include/common.h
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include <QtCore/QDebug>
+#include <QtCore/QString>
+
 namespace rust {
 namespace cxxqtlib1 {
 
@@ -22,6 +25,21 @@ void
 drop(T& value)
 {
   value.~T();
+}
+
+template<typename T>
+QString
+toQString(const T& value)
+{
+  // We can't convert value directly into a string.
+  // However most Qt types are able to stream into a QDebug object such as
+  // qDebug() << value We can then construct a QDebug object that outputs into a
+  // string (instead of logging), and return that string Thus we have a pretty
+  // reliable and generic "toString" implementation for most Qt types
+  QString res;
+  QDebug serializer{ &res };
+  serializer << value;
+  return res;
 }
 
 } // namespace cxxqtlib1
