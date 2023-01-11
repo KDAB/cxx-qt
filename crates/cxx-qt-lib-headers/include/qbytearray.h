5d45e20ab788a8536a396fa252f2da292bb56e18
Andrew Hayzen <andrew.hayzen@kdab.com>
2023-01-11 11:29:31 +0000

    cxx-qt-lib: add mutable methods to QByteArray for high performance data

--- crates/cxx-qt-lib-headers/include/qbytearray.h
+++ crates/cxx-qt-lib-headers/include/qbytearray.h
@@ -25,10 +25,27 @@ qbytearrayFromSliceU8(::rust::Slice<const ::std::uint8_t> slice);
 ::rust::Vec<::std::uint8_t>
 qbytearrayToVecU8(const QByteArray& byteArray);
 
+::rust::Slice<::std::uint8_t>
+qbytearrayAsMutSlice(QByteArray& byteArray);
 QByteArray
 qbytearrayFromRawData(::rust::Slice<const ::std::uint8_t> slice);
 ::rust::Slice<const ::std::uint8_t>
 qbytearrayAsSlice(const QByteArray& byteArray);
+
+void
+qbytearrayAppend(QByteArray& byteArray, ::std::uint8_t ch);
+void
+qbytearrayFill(QByteArray& byteArray, ::std::uint8_t ch, ::rust::isize size);
+void
+qbytearrayInsert(QByteArray& byteArray, ::rust::isize pos, ::std::uint8_t ch);
+::rust::isize
+qbytearrayLen(const QByteArray& byteArray);
+void
+qbytearrayRemove(QByteArray& byteArray, ::rust::isize pos, ::rust::isize len);
+void
+qbytearrayReserve(QByteArray& byteArray, ::rust::isize size);
+void
+qbytearrayResize(QByteArray& byteArray, ::rust::isize size);
 
 }
 }
