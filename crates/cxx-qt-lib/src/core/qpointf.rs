db7ed65a449107e7c4a939919b83b5280f9d7a2f
Andrew Hayzen <andrew.hayzen@kdab.com>
2023-01-17 14:00:29 +0000

    cxx-qt-lib: add methods for QPointF
    Related to #55

--- crates/cxx-qt-lib/src/core/qpointf.rs
+++ crates/cxx-qt-lib/src/core/qpointf.rs
@@ -10,23 +10,50 @@ use std::fmt;
 #[cxx::bridge]
 mod ffi {
     unsafe extern "C++" {
+        include!("cxx-qt-lib/qpoint.h");
+        type QPoint = crate::QPoint;
         include!("cxx-qt-lib/qpointf.h");
-        include!("cxx-qt-lib/qstring.h");
-
         type QPointF = super::QPointF;
+        include!("cxx-qt-lib/qstring.h");
         type QString = crate::QString;
+
+        /// Returns true if both the x and y coordinates are set to 0.0 (ignoring the sign); otherwise returns false.
+        #[rust_name = "is_null"]
+        fn isNull(self: &QPointF) -> bool;
+
+        /// Returns the sum of the absolute values of x() and y(),
+        /// traditionally known as the "Manhattan length" of the vector from the origin to the point.
+        #[rust_name = "manhattan_length"]
+        fn manhattanLength(self: &QPointF) -> f64;
+
+        /// Sets the x coordinate of this point to the given finite x coordinate.
+        #[rust_name = "set_x"]
+        fn setX(self: &mut QPointF, x: f64);
+
+        /// Sets the y coordinate of this point to the given finite y coordinate.
+        #[rust_name = "set_y"]
+        fn setY(self: &mut QPointF, y: f64);
+
+        /// Rounds the coordinates of this point to the nearest integer,
+        /// and returns a QPoint object with the rounded coordinates.
+        #[rust_name = "to_point"]
+        fn toPoint(self: &QPointF) -> QPoint;
+
+        /// Returns a point with x and y coordinates exchanged
+        fn transposed(self: &QPointF) -> QPointF;
 
         /// Returns the x coordinate of this point.
         fn x(self: &QPointF) -> f64;
+
         /// Returns the y coordinate of this point.
         fn y(self: &QPointF) -> f64;
+    }
 
-        /// Sets the x coordinate of this point to the given x coordinate.
-        #[rust_name = "set_x"]
-        fn setX(self: &mut QPointF, x: f64);
-        /// Sets the y coordinate of this point to the given y coordinate.
-        #[rust_name = "set_y"]
-        fn setY(self: &mut QPointF, y: f64);
+    #[namespace = "rust::cxxqtlib1"]
+    unsafe extern "C++" {
+        #[doc(hidden)]
+        #[rust_name = "qpointf_dot_product"]
+        fn qpointfDotProduct(p1: &QPointF, p2: &QPointF) -> f64;
     }
 
     #[namespace = "rust::cxxqtlib1"]
@@ -39,6 +66,9 @@ mod ffi {
         #[doc(hidden)]
         #[rust_name = "qpointf_init"]
         fn construct(x: f64, y: f64) -> QPointF;
+        #[doc(hidden)]
+        #[rust_name = "qpointf_from_qpoint"]
+        fn construct(point: &QPoint) -> QPointF;
         #[doc(hidden)]
         #[rust_name = "qpointf_to_qstring"]
         fn toQString(value: &QPointF) -> QString;
@@ -66,6 +96,11 @@ pub struct QPointF {
 }
 
 impl QPointF {
+    /// Returns the dot product of p1 and p2.
+    pub fn dot_product(p1: &QPointF, p2: &QPointF) -> f64 {
+        ffi::qpointf_dot_product(p1, p2)
+    }
+
     /// Constructs a point with the given coordinates (x, y).
     pub fn new(x: f64, y: f64) -> Self {
         ffi::qpointf_init(x, y)
@@ -82,6 +117,21 @@ impl Default for QPointF {
 impl fmt::Display for QPointF {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", ffi::qpointf_to_qstring(self))
+    }
+}
+
+impl From<&ffi::QPoint> for QPointF {
+    /// Constructs a copy of the given point.
+    fn from(point: &ffi::QPoint) -> Self {
+        ffi::qpointf_from_qpoint(point)
+    }
+}
+
+impl From<QPointF> for ffi::QPoint {
+    /// Rounds the coordinates of this point to the nearest integer,
+    /// and returns a QPoint object with the rounded coordinates.
+    fn from(value: QPointF) -> Self {
+        value.to_point()
     }
 }
 
