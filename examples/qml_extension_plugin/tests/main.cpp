c88e2fc7e57b25f970ecbaa5ef7fbac214be7115
Leon Matthes <leon.matthes@kdab.com>
2023-02-23 11:48:02 +0100

    Remove qmltestrunner dependency
    Use QTest directly, this fixes the build on my Fedora machine, which
    only provides qmltestrunner linked with Qt5.
    This might fix #110 as well.

---
+++ examples/qml_extension_plugin/tests/main.cpp
@@ -0,0 +1,9 @@
+// clang-format off
+// SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// clang-format on
+// SPDX-FileContributor: Leon Matthes <leon.matthes@kdab.com>
+
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+#include <QtQuickTest>
+QUICK_TEST_MAIN(qml_extension_plugin)
