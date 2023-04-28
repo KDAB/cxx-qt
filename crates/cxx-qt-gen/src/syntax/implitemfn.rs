527c9e7dbbc1138e8400c79951db4817520ad593
Andrew Hayzen <andrew.hayzen@kdab.com>
2023-04-28 13:31:37 +0100

    cxx-qt-gen: change is_method_mutable to check only for Pin<&mut Self>

--- crates/cxx-qt-gen/src/syntax/implitemfn.rs
+++ crates/cxx-qt-gen/src/syntax/implitemfn.rs
@@ -5,14 +5,14 @@
 
 use syn::{FnArg, Receiver, Signature};
 
-use super::types::is_pin_mut;
+use super::types::{is_pin_mut, is_pin_of_self};
 
-/// Return whether the first parameter of a method is a mutable self argument
-pub fn is_method_mutable(signature: &Signature) -> bool {
+/// Return whether the first parameter of a method is Pin<&mut Self>
+pub fn is_method_mutable_pin_of_self(signature: &Signature) -> bool {
     match signature.inputs.first() {
-        Some(FnArg::Receiver(Receiver { mutability, ty, .. })) => {
-            // Check if Pin<T> is mut or fallback to normal mutability
-            is_pin_mut(ty) || mutability.is_some()
+        Some(FnArg::Receiver(Receiver { ty, .. })) => {
+            // Check if Pin<T> is mut and T is self
+            is_pin_mut(ty) && is_pin_of_self(ty)
         }
         _ => false,
     }
@@ -28,16 +28,30 @@ mod tests {
 
     #[test]
     fn test_is_method_mutable_self() {
-        assert!(!is_method_mutable(
+        assert!(!is_method_mutable_pin_of_self(
             &tokens_to_syn::<ImplItemFn>(quote! {
                 fn invokable(&self) {}
             })
             .sig
         ));
 
-        assert!(is_method_mutable(
+        assert!(!is_method_mutable_pin_of_self(
+            &tokens_to_syn::<ImplItemFn>(quote! {
+                fn invokable(&mut self) {}
+            })
+            .sig
+        ));
+
+        assert!(is_method_mutable_pin_of_self(
             &tokens_to_syn::<ImplItemFn>(quote! {
                 fn invokable_with_return_cxx_type(self: Pin<&mut Self>) -> f64 {}
+            })
+            .sig
+        ));
+
+        assert!(is_method_mutable_pin_of_self(
+            &tokens_to_syn::<ImplItemFn>(quote! {
+                fn invokable_with_return_cxx_type(mut self: Pin<&mut Self>) -> f64 {}
             })
             .sig
         ));
@@ -45,21 +59,28 @@ mod tests {
 
     #[test]
     fn test_is_method_mutable_value() {
-        assert!(!is_method_mutable(
+        assert!(!is_method_mutable_pin_of_self(
             &tokens_to_syn::<ImplItemFn>(quote! {
                 fn invokable(value: T) {}
             })
             .sig
         ));
 
-        assert!(!is_method_mutable(
+        assert!(!is_method_mutable_pin_of_self(
             &tokens_to_syn::<ImplItemFn>(quote! {
                 fn invokable_with_return_cxx_type(value: Pin<&mut T>) -> f64 {}
             })
             .sig
         ));
 
-        assert!(!is_method_mutable(
+        assert!(!is_method_mutable_pin_of_self(
+            &tokens_to_syn::<ImplItemFn>(quote! {
+                fn invokable_with_return_cxx_type(mut value: Pin<&mut T>) -> f64 {}
+            })
+            .sig
+        ));
+
+        assert!(!is_method_mutable_pin_of_self(
             &tokens_to_syn::<ImplItemFn>(quote! {
                 fn invokable_with_return_cxx_type(mut value: T) -> f64 {}
             })
