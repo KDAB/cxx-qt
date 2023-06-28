61a9291f5582f662a0a353a6c357df7430d0a904
Andrew Hayzen <andrew.hayzen@kdab.com>
2023-06-28 18:38:40 +0100

    examples: create a multi project to test multiple crates

---
+++ examples/meta_project/cpp/main.cpp
@@ -0,0 +1,31 @@
+// clang-format off
+// SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// clang-format on
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+
+int
+main(int argc, char* argv[])
+{
+  QGuiApplication app(argc, argv);
+
+  QQmlApplicationEngine engine;
+
+  const QUrl url(QStringLiteral("qrc:/main.qml"));
+  QObject::connect(
+    &engine,
+    &QQmlApplicationEngine::objectCreated,
+    &app,
+    [url](QObject* obj, const QUrl& objUrl) {
+      if (!obj && url == objUrl)
+        QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+
+  engine.load(url);
+
+  return app.exec();
+}
