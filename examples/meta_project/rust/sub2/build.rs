56b5e37e8ff0f65a8659167ad56fa806b0fb9f6c
Andrew Hayzen <andrew.hayzen@kdab.com>
2023-06-28 18:38:40 +0100

    examples: create a multi project to test multiple crates

---
+++ examples/meta_project/rust/sub2/build.rs
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use cxx_qt_build::{CxxQtBuilder, QmlModule};
+
+fn main() {
+    CxxQtBuilder::new()
+        .qml_module(QmlModule::<_, &str> {
+            uri: "com.kdab.cxx_qt.demo.sub2",
+            rust_files: &["src/sub2_object.rs"],
+            ..Default::default()
+        })
+        .build();
+}
