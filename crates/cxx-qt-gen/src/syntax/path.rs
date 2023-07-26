b035fd963d6acd97b58f92305352a097912978e9
Andrew Hayzen <andrew.hayzen@kdab.com>
2023-07-26 14:21:56 +0100

    cxx-qt-gen: parse all items and use Path::get_ident instead of helper
    This then allows us to easily parse extern "C++Qt" later.
    
    Related to #577

--- crates/cxx-qt-gen/src/syntax/path.rs
+++ crates/cxx-qt-gen/src/syntax/path.rs
@@ -3,7 +3,7 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use syn::{spanned::Spanned, Error, Ident, Path, Result};
+use syn::Path;
 
 /// Returns whether the [syn::Path] matches a given string slice
 pub fn path_compare_str(path: &Path, string: &[&str]) -> bool {
@@ -20,18 +20,6 @@ pub fn path_compare_str(path: &Path, string: &[&str]) -> bool {
             == 0
 }
 
-/// Returns the first [syn::Ident] from the [syn::Path] segments, errors if there are none or many
-pub fn path_to_single_ident(path: &Path) -> Result<Ident> {
-    if path.segments.len() == 1 {
-        Ok(path.segments[0].ident.clone())
-    } else {
-        Err(Error::new(
-            path.span(),
-            "Expected only one segment in the Path",
-        ))
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use syn::parse_quote;
@@ -45,15 +33,5 @@ mod tests {
         assert!(path_compare_str(&path, &["a", "b", "c"]));
         assert!(!path_compare_str(&path, &["a", "c", "b"]));
         assert!(!path_compare_str(&path, &["a", "b", "c", "d"]));
-    }
-
-    #[test]
-    fn test_path_to_single_ident() {
-        let path: Path = parse_quote! { a::b::c };
-        assert!(path_to_single_ident(&path).is_err());
-
-        let path: Path = parse_quote! { a };
-        let ident = path_to_single_ident(&path).unwrap();
-        assert_eq!(ident, "a");
     }
 }
