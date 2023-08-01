9afa9fb1f18476d2d0c0fcc3cf6488200e2ff123
Cyril Jacquet <cyril.jacquet@skribisto.eu>
2023-08-01 14:40:34 +0200

    add compile flag /bigobj for MSVC

--- crates/cxx-qt-build/src/lib.rs
+++ crates/cxx-qt-build/src/lib.rs
@@ -497,8 +497,10 @@ impl CxxQtBuilder {
             builder.flag_if_supported("/std:c++17");
             builder.flag_if_supported("/Zc:__cplusplus");
             builder.flag_if_supported("/permissive-");
+            builder.flag_if_supported("/bigobj");
             // GCC + Clang
             builder.flag_if_supported("-std=c++17");
+            // MinGW requires big-obj otherwise debug builds fail
             builder.flag_if_supported("-Wa,-mbig-obj");
             // Enable Qt Gui in C++ if the feature is enabled
             #[cfg(feature = "qt_gui")]
--- crates/cxx-qt-lib/build.rs
+++ crates/cxx-qt-lib/build.rs
@@ -268,8 +268,11 @@ fn main() {
     builder.flag_if_supported("/std:c++17");
     builder.flag_if_supported("/Zc:__cplusplus");
     builder.flag_if_supported("/permissive-");
+    builder.flag_if_supported("/bigobj");
+
     // GCC + Clang
     builder.flag_if_supported("-std=c++17");
+    // MinGW requires big-obj otherwise debug builds fail
     builder.flag_if_supported("-Wa,-mbig-obj");
 
     builder.compile("cxx-qt-lib");
