d16bfe4c05ef69ab3a66b4814bfadab53d99e07f
Laurent Montel <montel@kde.org>
2024-01-04 15:25:54 +0100

    Fix build in qt5

--- crates/cxx-qt-lib/src/gui/qpen.cpp
+++ crates/cxx-qt-lib/src/gui/qpen.cpp
@@ -12,15 +12,9 @@
 
 // https://code.qt.io/cgit/qt/qtbase.git/tree/src/gui/painting/qpen.h?h=v5.15.6-lts-lgpl#n124
 // https://code.qt.io/cgit/qt/qtbase.git/tree/src/gui/painting/qpen.h?h=v6.2.4#n94
-#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
-assert_alignment_and_size(QPen,
-                          alignof(::std::size_t),
-                          sizeof(::std::size_t) * 2);
-#else
 assert_alignment_and_size(QPen,
                           alignof(::std::size_t),
                           sizeof(::std::size_t));
-#endif
 
 static_assert(!::std::is_trivially_copy_assignable<QPen>::value);
 static_assert(!::std::is_trivially_copy_constructible<QPen>::value);
