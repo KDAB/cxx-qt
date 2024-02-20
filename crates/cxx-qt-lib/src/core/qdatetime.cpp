d03d11e1a80b3fea1e288bb5bb4801c2f49c990a
Laurent Montel <laurent@kdab.com>
2024-02-20 12:45:02 +0100

    Fix clang-format

--- crates/cxx-qt-lib/src/core/qdatetime.cpp
+++ crates/cxx-qt-lib/src/core/qdatetime.cpp
@@ -149,7 +149,8 @@ void
 qdatetimeSetTimeZone(QDateTime& datetime, const QTimeZone& timeZone)
 {
 #if (QT_VERSION >= QT_VERSION_CHECK(6, 7, 0))
-  datetime.setTimeZone(timeZone, QDateTime::TransitionResolution::LegacyBehavior);
+  datetime.setTimeZone(timeZone,
+                       QDateTime::TransitionResolution::LegacyBehavior);
 #else
   datetime.setTimeZone(timeZone);
 #endif
