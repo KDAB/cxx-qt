4913b4c18a46562d40d35c5d21a0d0fddacdbbf5
Laurent Montel <laurent@kdab.com>
2024-02-22 16:08:29 +0100

    Add qcommandlineparser

--- crates/cxx-qt-lib-extras/build.rs
+++ crates/cxx-qt-lib-extras/build.rs
@@ -21,13 +21,13 @@ fn main() {
             .major
     );
 
-    let rust_bridges = vec!["core/qcommandlineoption"];
+    let rust_bridges = vec!["core/qcommandlineoption", "core/qcommandlineparser"];
 
     for rust_source in &rust_bridges {
         builder = builder.file(format!("src/{rust_source}.rs"));
     }
 
-    let cpp_files = vec!["core/qcommandlineoption"];
+    let cpp_files = vec!["core/qcommandlineoption", "core/qcommandlineparser"];
 
     builder = builder.cc_builder(move |cc| {
         for cpp_file in &cpp_files {
