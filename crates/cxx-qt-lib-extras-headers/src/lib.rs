bd2dcc3d18631dd802195f6a8dca7800bd895b40
Laurent Montel <laurent@kdab.com>
2024-02-22 11:28:00 +0100

    Fix compile

--- crates/cxx-qt-lib-extras-headers/src/lib.rs
+++ crates/cxx-qt-lib-extras-headers/src/lib.rs
@@ -3,8 +3,6 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-#![deny(missing_docs)]
-
 /// Retrieves the headers for cxx-qt-lib-extras
 ///
 /// These can be passed into [cxx_qt_build::CxxQtBuilder].
