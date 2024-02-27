8ca46a68ec91a65b267ae8c85e3c156b664c8e8b
Laurent Montel <laurent@kdab.com>
2024-02-27 11:06:20 +0100

    Fix coding style

--- tests/qt_types_standalone/cpp/qpolygon.h
+++ tests/qt_types_standalone/cpp/qpolygon.h
@@ -23,11 +23,11 @@ private Q_SLOTS:
   }
   void clone()
   {
-#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))	  
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
     const auto m = QPolygon(QList<QPoint>() << QPoint(1, 2) << QPoint(3, 4));
 #else
     const auto m = QPolygon(QVector<QPoint>() << QPoint(1, 2) << QPoint(3, 4));
-#endif    
+#endif
     const auto c = clone_qpolygon(m);
     QCOMPARE(c.point(0), { QPoint(1, 2) });
     QCOMPARE(c.point(1), QPoint(3, 4));
