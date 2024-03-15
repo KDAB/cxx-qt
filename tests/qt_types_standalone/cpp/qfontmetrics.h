0fa3bbf863ad0c52bcf2d6ecaeb9e5aa7bef2f88
Laurent Montel <laurent@kdab.com>
2024-03-15 15:31:28 +0100

    Add qfontmetrics test

---
+++ tests/qt_types_standalone/cpp/qfontmetrics.h
@@ -0,0 +1,34 @@
+// clang-format off
+// SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// clang-format on
+// SPDX-FileContributor: Laurent Montel <laurent.montel@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+#pragma once
+
+#include <QtGui/QFontMetrics>
+#include <QtGui/QFont>
+#include <QtTest/QTest>
+
+#include "cxx-qt-gen/qfontmetrics.cxx.h"
+
+class QFontMetricsTest : public QObject
+{
+  Q_OBJECT
+
+private Q_SLOTS:
+  void construct()
+  {
+  }
+  void clone()
+  {
+    QFont f;
+    f.setBold(true);
+    f.setPointSize(30);
+    const auto m = QFontMetrics(f);
+
+    const auto c = clone_qfontmetrics(m);
+    QCOMPARE(m.ascent(), c.ascent());
+    QCOMPARE(m.height(), c.height());
+  }
+};
