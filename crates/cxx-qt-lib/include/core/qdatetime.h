89ba58e3b03898ccb497d29ed451a1e4402c93ac
Andrew Hayzen <andrew.hayzen@kdab.com>
2024-03-28 17:17:04 +0000

    cxx-qt-lib: use qint64 type for QDate and QDateTime

--- crates/cxx-qt-lib/include/core/qdatetime.h
+++ crates/cxx-qt-lib/include/core/qdatetime.h
@@ -29,43 +29,23 @@ namespace rust {
 namespace cxxqtlib1 {
 
 QDateTime
-qdatetimeAddDays(const QDateTime& datetime, ::std::int64_t ndays);
-QDateTime
-qdatetimeAddMSecs(const QDateTime& datetime, ::std::int64_t msecs);
-QDateTime
-qdatetimeAddSecs(const QDateTime& datetime, ::std::int64_t secs);
-QDateTime
 qdatetimeCurrentDateTime();
 QDateTime
 qdatetimeCurrentDateTimeUtc();
-::std::int64_t
+qint64
 qdatetimeCurrentMSecsSinceEpoch();
-::std::int64_t
+qint64
 qdatetimeCurrentSecsSinceEpoch();
-::std::int64_t
-qdatetimeDaysTo(const QDateTime& datetime, const QDateTime& other);
 QDateTime
-qdatetimeFromMSecsSinceEpoch(::std::int64_t msecs, const QTimeZone& timeZone);
+qdatetimeFromMSecsSinceEpoch(qint64 msecs, const QTimeZone& timeZone);
 QDateTime
-qdatetimeFromSecsSinceEpoch(::std::int64_t secs, const QTimeZone& timeZone);
-::std::int64_t
-qdatetimeMSecsTo(const QDateTime& datetime, const QDateTime& other);
-::std::int64_t
-qdatetimeSecsTo(const QDateTime& datetime, const QDateTime& other);
+qdatetimeFromSecsSinceEpoch(qint64 secs, const QTimeZone& timeZone);
 void
 qdatetimeSetDate(QDateTime& datetime, QDate date);
-void
-qdatetimeSetMSecsSinceEpoch(QDateTime& datetime, ::std::int64_t msecs);
-void
-qdatetimeSetSecsSinceEpoch(QDateTime& datetime, ::std::int64_t secs);
 void
 qdatetimeSetTime(QDateTime& datetime, QTime time);
 ::std::unique_ptr<QTimeZone>
 qdatetimeTimeZone(const QDateTime& datetime);
-::std::int64_t
-qdatetimeToMSecsSinceEpoch(const QDateTime& datetime);
-::std::int64_t
-qdatetimeToSecsSinceEpoch(const QDateTime& datetime);
 void
 qdatetimeSetTimeZone(QDateTime& datetime, const QTimeZone& timeZone);
 QDateTime
