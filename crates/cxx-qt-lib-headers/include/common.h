63f3f838e9c0ed55c96a4a1018152808ab53eb6b
knox <knoxfighter@asdff45.de>
2024-03-29 16:43:16 +0100

    forward args to make_shared/unique

--- crates/cxx-qt-lib-headers/include/common.h
+++ crates/cxx-qt-lib-headers/include/common.h
@@ -91,16 +91,16 @@ operatorDiv(const S scalar, const T& t)
 
 template<typename T, typename... Args>
 std::unique_ptr<T>
-make_unique(Args... args)
+make_unique(Args&&... args)
 {
-  return std::make_unique<T>(args...);
+  return std::make_unique<T>(std::forward<Args>(args)...);
 }
 
 template<typename T, typename... Args>
 std::shared_ptr<T>
-make_shared(Args... args)
+make_shared(Args&&... args)
 {
-  return std::make_shared<T>(args...);
+  return std::make_shared<T>(std::forward<Args>(args)...);
 }
 
 } // namespace cxxqtlib1
