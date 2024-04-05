9caf4777696bf4abcd6b127147feb567f2f3f8ed
Montel Laurent <laurent.montel@kdab.com>
2024-04-05 13:02:23 +0200

    Add cxx qt lib extras (#860)
    * Create cxx-qt-lib-extras crates
    
    * Add QCommandLineOption support in qcommandlineparser
    
    * Add qelapsedtimer

---
+++ crates/cxx-qt-lib-extras/src/core/qcommandlineparser.cpp
@@ -0,0 +1,45 @@
+// clang-format off
+// SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// clang-format on
+// SPDX-FileContributor: Laurent Montel <laurent.montel@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+#include "cxx-qt-lib-extras/qcommandlineparser.h"
+
+#include "../assertion_utils.h"
+
+// QCommandLineParser has a single pointer as it's member
+//
+// https://code.qt.io/cgit/qt/qtbase.git/tree/src/corelib/tools/qcommandlineparser.h?h=v5.15.6-lts-lgpl#n107
+// https://code.qt.io/cgit/qt/qtbase.git/tree/src/corelib/tools/qcommandlineparser.h?h=v6.2.4#n109
+assert_alignment_and_size(QCommandLineParser,
+                          alignof(::std::size_t),
+                          sizeof(::std::size_t));
+
+static_assert(!::std::is_trivially_copy_assignable<QCommandLineParser>::value);
+
+namespace rust {
+namespace cxxqtlib1 {
+QString
+qcommandlineparserValue(const QCommandLineParser& parser,
+                        const QString& optionName)
+{
+  return parser.value(optionName);
+}
+
+QStringList
+qcommandlineparserValues(const QCommandLineParser& parser,
+                         const QString& optionName)
+{
+  return parser.values(optionName);
+}
+
+bool
+qcommandlineparserIsSetFromQString(const QCommandLineParser& parser,
+                                   const QString& optionName)
+{
+  return parser.isSet(optionName);
+}
+
+}
+}
