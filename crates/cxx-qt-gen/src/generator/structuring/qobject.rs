27742a965ac65cc7490600cb9e5c78aafa17f929
Ben Ford <ben.ford@kdab.com>
2024-08-01 14:01:41 +0100

    Create fields in StructuredQObject and ParsedCxxQtData for inherited methods
    Also associate inherited methods from cxxqtdata with StructuredQObjects in structuring phase

--- crates/cxx-qt-gen/src/generator/structuring/qobject.rs
+++ crates/cxx-qt-gen/src/generator/structuring/qobject.rs
@@ -3,6 +3,7 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use crate::parser::inherit::ParsedInheritedMethod;
 use crate::parser::method::ParsedMethod;
 use crate::parser::signals::ParsedSignal;
 use crate::parser::{qenum::ParsedQEnum, qobject::ParsedQObject};
@@ -14,6 +15,7 @@ pub struct StructuredQObject<'a> {
     pub declaration: &'a ParsedQObject,
     pub qenums: Vec<&'a ParsedQEnum>,
     pub methods: Vec<&'a ParsedMethod>,
+    pub inherited_methods: Vec<&'a ParsedInheritedMethod>,
     pub signals: Vec<&'a ParsedSignal>,
 }
 
@@ -28,6 +30,7 @@ impl<'a> StructuredQObject<'a> {
             declaration: qobject,
             qenums: vec![],
             methods: vec![],
+            inherited_methods: vec![],
             signals: vec![],
         }
     }
