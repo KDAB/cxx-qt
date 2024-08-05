3da866427cf8ba7cb20dcc329d3bc20acb4d4c1f
BenFordTytherington <110855418+BenFordTytherington@users.noreply.github.com>
2024-08-05 10:59:42 +0100

    Create script for generating data and reports for code coverage
    Create grcov script and modify gitignore to exclude .profraw directories

---
+++ scripts/grcov_cxx_qt.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2021 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+# SPDX-FileContributor: Ben Ford <ben.ford@kdab.com>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+# Assumes you have grcov and llvm-tools
+# Install:
+# cargo install grcov
+# rustup component add llvm-tools
+set -ex
+# Ensure we are in the right directory
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH/../"
+
+export RUSTFLAGS="-Cinstrument-coverage"
+export LLVM_PROFILE_FILE="$SCRIPTPATH/coverage/coverage_data-%p-%m.profraw"
+cargo build --package cxx-qt-gen
+cargo test --package cxx-qt-gen
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/
+echo "Coverage html report generated in $(realpath "$SCRIPTPATH"/../target/debug/html)"
