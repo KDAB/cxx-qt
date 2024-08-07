81dc4971855fe1d145572224286b98960bf64d57
Ben Ford <ben.ford@kdab.com>
2024-08-07 15:55:12 +0100

    Resolve nitpicks

--- crates/cxx-qt-gen/test_inputs/shebang.rs
+++ crates/cxx-qt-gen/test_inputs/shebang.rs
@@ -1,5 +1,13 @@
 #!/usr/bin/
 // Test for qtitem and qtfile, for code coverage
-
 #[cxx_qt::bridge]
-mod ffi {}
+mod ffi {
+    extern "RustQt" {
+        #[qobject]
+        type MyObject = super::MyObjectRust;
+    }
+
+    unsafe extern "RustQt" {
+        fn my_fn(self: &MyObject);
+    }
+}
