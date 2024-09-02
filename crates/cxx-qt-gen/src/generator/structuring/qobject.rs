24b6a1b679b174317e57c4514f59c07b1ff45887
Ben Ford <ben.ford@kdab.com>
2024-09-02 12:26:47 +0100

    Format merge commit and fix some imports

--- crates/cxx-qt-gen/src/generator/structuring/qobject.rs
+++ crates/cxx-qt-gen/src/generator/structuring/qobject.rs
@@ -57,14 +57,13 @@ impl<'a> StructuredQObject<'a> {
     pub fn method_lookup(&self, id: &Ident) -> Result<Name> {
         lookup(&self.methods, id, |method| &method.name)
             .or_else(|| lookup(&self.inherited_methods, id, |inherited| &inherited.name)) // fallback to searching inherited methods
-            .ok_or_else(|| Error::new_spanned(id, format!("Method with name '{id}' not found!")))
-
+            .ok_or_else(|| not_found_error("Method", id))
     }
 
     /// Returns the name of the signal with the provided Rust ident if it exists, or an error
     pub fn signal_lookup(&self, id: &Ident) -> Result<Name> {
         lookup(&self.signals, id, |signal| &signal.name)
-            .ok_or_else(|| Error::new_spanned(id, format!("Signal with name '{id}' not found!")))
+            .ok_or_else(|| not_found_error("Signal", id))
     }
 
     #[cfg(test)]
