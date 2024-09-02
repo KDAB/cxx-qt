140078cb01a9963e6515f2aa64e4d5fe131097ea
Sander Vocke <sandervocke@gmail.com>
2024-09-02 13:33:14 +0200

    Change to auto-generated files

--- crates/cxx-qt-lib/src/core/qlist/qlist_qline.rs
+++ crates/cxx-qt-lib/src/core/qlist/qlist_qline.rs
@@ -1,8 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
-// SPDX-FileContributor: Sander Vocke (sandervocke@gmail.com)
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
-
 
 #[cxx::bridge]
 pub mod ffi {
--- crates/cxx-qt-lib/src/core/qlist/qlist_qlinef.rs
+++ crates/cxx-qt-lib/src/core/qlist/qlist_qlinef.rs
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
-// SPDX-FileContributor: Sander Vocke (sandervocke@gmail.com)
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
