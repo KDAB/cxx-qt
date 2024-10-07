050a93a002e975fdb87e65db7e5ad6e73e42657d
Leon Matthes <leon.matthes@kdab.com>
2024-10-07 11:03:38 +0200

    Rename full_qt feature to qt_full.
    This is in line with our other feature names, like qt_gui, etc.

--- examples/cargo_without_cmake/build.rs
+++ examples/cargo_without_cmake/build.rs
@@ -10,8 +10,8 @@ fn main() {
     CxxQtBuilder::new()
         // Link Qt's Network library
         // - Qt Core is always linked
-        // - Qt Gui is linked by enabling the qt_gui Cargo feature (default).
-        // - Qt Qml is linked by enabling the qt_qml Cargo feature (default).
+        // - Qt Gui is linked by enabling the qt_gui Cargo feature of cxx-qt-lib.
+        // - Qt Qml is linked by enabling the qt_qml Cargo feature of cxx-qt-lib.
         // - Qt Qml requires linking Qt Network on macOS
         .qt_module("Network")
         .qml_module(QmlModule {
