396b26f9996efad8f601eaa7681d0af316b7e726
Andrew Hayzen <andrew.hayzen@kdab.com>
2024-10-15 14:28:40 +0100

    cxx-qt: use cxx-qt-build and reexport from cxx-qt-lib
    This removes a build depends of cxx-qt-build on cxx-qt

--- crates/cxx-qt/build.rs
+++ crates/cxx-qt/build.rs
@@ -3,60 +3,55 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use std::{fs::File, io::Write};
+use std::path::PathBuf;
+
+use cxx_qt_build::{CxxQtBuilder, Interface};
+
+fn header_dir() -> PathBuf {
+    PathBuf::from(std::env::var("OUT_DIR").unwrap())
+        .join("include")
+        .join("cxx-qt")
+}
+
+fn write_headers() {
+    println!("cargo::rerun-if-changed=include/");
+    std::fs::create_dir_all(header_dir()).expect("Failed to create include directory");
+
+    for file_path in [
+        "connection.h",
+        "signalhandler.h",
+        "thread.h",
+        "threading.h",
+        "type.h",
+    ] {
+        println!("cargo::rerun-if-changed=include/{file_path}");
+        std::fs::copy(format!("include/{file_path}"), header_dir().join(file_path))
+            .expect("Failed to copy header file!");
+    }
+}
 
 fn main() {
-    let qtbuild = qt_build_utils::QtBuild::new(vec!["Core".to_owned()])
-        .expect("Could not find Qt installation");
+    write_headers();
 
-    // Required for tests
-    qt_build_utils::setup_linker();
+    let interface = Interface::default()
+        .export_include_prefixes([])
+        .export_include_directory(header_dir(), "cxx-qt");
+
+    let mut builder = CxxQtBuilder::library(interface);
 
     let cpp_files = ["src/connection.cpp"];
     let rust_bridges = ["src/connection.rs"];
 
     for bridge in &rust_bridges {
-        println!("cargo:rerun-if-changed={bridge}");
+        builder = builder.file(bridge);
     }
 
-    let mut builder = cxx_build::bridges(rust_bridges);
+    builder = builder.cc_builder(move |cc| {
+        for cpp_file in &cpp_files {
+            cc.file(cpp_file);
+            println!("cargo::rerun-if-changed={cpp_file}");
+        }
+    });
 
-    qtbuild.cargo_link_libraries(&mut builder);
-
-    for cpp_file in &cpp_files {
-        builder.file(cpp_file);
-        println!("cargo:rerun-if-changed={cpp_file}");
-    }
-
-    // Write this library's manually written C++ headers to files and add them to include paths
-    let out_dir = std::env::var("OUT_DIR").unwrap();
-    let directory = format!("{out_dir}/cxx-qt");
-    std::fs::create_dir_all(&directory).expect("Could not create cxx-qt header directory");
-    // Note we only need connection.h for now, but lets move all headers to be consistent
-    // ensure src/lib write_headers is consistent
-    for (file_contents, file_name) in [
-        (include_str!("include/connection.h"), "connection.h"),
-        (include_str!("include/signalhandler.h"), "signalhandler.h"),
-        (include_str!("include/thread.h"), "thread.h"),
-        (include_str!("include/threading.h"), "threading.h"),
-        (include_str!("include/type.h"), "type.h"),
-    ] {
-        let h_path = format!("{directory}/{file_name}");
-        let mut header = File::create(h_path).expect("Could not create cxx-qt header");
-        write!(header, "{file_contents}").expect("Could not write cxx-qt header");
-    }
-    builder.include(out_dir);
-    builder.includes(qtbuild.include_paths());
-
-    // Note, ensure our settings stay in sync across cxx-qt, cxx-qt-build, and cxx-qt-lib
-    builder.cpp(true);
-    builder.std("c++17");
-    // MSVC
-    builder.flag_if_supported("/Zc:__cplusplus");
-    builder.flag_if_supported("/permissive-");
-    builder.flag_if_supported("/bigobj");
-    // MinGW requires big-obj otherwise debug builds fail
-    builder.flag_if_supported("-Wa,-mbig-obj");
-
-    builder.compile("cxx-qt");
+    builder.build();
 }
