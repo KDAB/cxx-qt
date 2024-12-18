f0f14cab1bb92fb83141a27ec63c2b147c6701a6
Leon Matthes <leon.matthes@kdab.com>
2024-12-18 11:51:07 +0100

    New example: ToDo app
    This is the example code developed during the tutorial videos.

---
+++ examples/todo_app/src/main.rs
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Leon Matthes <leon.matthes@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+mod todo_list;
+
+use cxx_qt_lib::{QGuiApplication, QQmlApplicationEngine, QUrl};
+
+fn main() {
+    let mut app = QGuiApplication::new();
+    let mut engine = QQmlApplicationEngine::new();
+
+    if let Some(engine) = engine.as_mut() {
+        engine.load(&QUrl::from("qrc:/qt/qml/com/kdab/todo/qml/main.qml"));
+    }
+
+    if let Some(app) = app.as_mut() {
+        app.exec();
+    }
+}
