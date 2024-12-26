729256ee3cbd2af7729befaff3a170157d10434b
Joshua Booth <joshua.n.booth@gmail.com>
2024-12-26 11:42:36 -0800

    cxx-qt-lib: add licensing header to quuid.rs

--- crates/cxx-qt-lib/src/core/quuid.rs
+++ crates/cxx-qt-lib/src/core/quuid.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Joshua Booth <joshua.n.booth@gmail.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
 use crate::QString;
 use cxx::{type_id, ExternType};
 use std::fmt;
