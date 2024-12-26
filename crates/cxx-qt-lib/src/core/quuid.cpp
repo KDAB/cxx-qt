c650c9df7c202fda8c2a8f15cfe8abcd85e2c301
Joshua Booth <joshua.n.booth@gmail.com>
2024-12-26 11:38:28 -0800

    remove superfluous SPDX-FileContributor comments

--- crates/cxx-qt-lib/src/core/quuid.cpp
+++ crates/cxx-qt-lib/src/core/quuid.cpp
@@ -1,7 +1,6 @@
 // clang-format off
 // SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 // clang-format on
-// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
 // SPDX-FileContributor: Joshua Booth <joshua.n.booth@gmail.com>
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
