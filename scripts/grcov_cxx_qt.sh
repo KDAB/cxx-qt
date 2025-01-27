ad6024776fcaadcec7545a99f96eaf42cdfbc3fd
Andrew Hayzen <andrew.hayzen@kdab.com>
2025-01-27 16:18:28 +0000

    WIP: cov: ensure we use host llvm version and clear coverage data

--- scripts/grcov_cxx_qt.sh
+++ scripts/grcov_cxx_qt.sh
@@ -5,19 +5,31 @@
 #
 # SPDX-License-Identifier: MIT OR Apache-2.0
 
-# Assumes you have grcov and llvm-tools
+# Assumes you have grcov and llvm in a system path
 # Install:
 # cargo install grcov
-# rustup component add llvm-tools
 set -ex
+
 # Ensure we are in the right directory
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH/../"
 
+# Ensure coverage folder is cleared
+rm -f "$SCRIPTPATH"/coverage/*.profraw
+
+# Check that the llvm path exists
+#
+# We can use rustup component add llvm-tools but this can be out of sync
+# See versions from the table in this link
+# https://github.com/taiki-e/cargo-llvm-cov?tab=readme-ov-file#get-coverage-of-cc-code-linked-to-rust-librarybinary
+if [ ! -d /usr/lib/llvm-17/bin/ ]; then
+  echo "LLVM 17 not found"
+fi
+
 export RUSTFLAGS="-Cinstrument-coverage"
 export LLVM_PROFILE_FILE="$SCRIPTPATH/coverage/coverage_data-%p-%m.profraw"
 cargo build --package cxx-qt-gen
 cargo test --package cxx-qt-gen
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/ --excl-start CODECOV_EXCLUDE_START --excl-stop CODECOV_EXCLUDE_STOP
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --llvm --llvm-path=/usr/lib/llvm-17/bin/ -o ./target/debug/ --excl-start CODECOV_EXCLUDE_START --excl-stop CODECOV_EXCLUDE_STOP
 echo "Coverage html report generated in $(realpath "$SCRIPTPATH"/../target/debug/html)"
