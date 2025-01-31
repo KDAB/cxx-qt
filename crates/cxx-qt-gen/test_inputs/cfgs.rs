fe7f8a2eb0cb5b15458050cd9321ebbcc809ac29
Andrew Hayzen <andrew.hayzen@kdab.com>
2025-01-31 17:06:09 +0000

    WIP: cxx-qt-gen: add more variants for snapshot test of cfgs

--- crates/cxx-qt-gen/test_inputs/cfgs.rs
+++ crates/cxx-qt-gen/test_inputs/cfgs.rs
@@ -1,38 +1,136 @@
 #[cxx_qt::bridge]
 mod ffi {
-    #[qenum(MyObject)]
-    #[cfg(not(enabled))]
-    enum EnumDisabled {
-        A,
+    // Enabled C++Qt QObject
+    // - disabled and enabled qsignal
+    unsafe extern "C++Qt" {
+        #[qobject]
+        #[cfg(enabled)]
+        type QObjectExternEnabled;
+
+        #[qsignal]
+        #[cfg(not(enabled))]
+        fn signal_disabled1(self: Pin<&mut QObjectExternEnabled>);
+
+        #[qsignal]
+        #[cfg(enabled)]
+        fn signal_enabled1(self: Pin<&mut QObjectExternEnabled>);
     }
 
+    // Disabled C++Qt QObject
+    // - disabled and enabled qsignal
     unsafe extern "C++Qt" {
         #[qobject]
         #[cfg(not(enabled))]
-        type QTimer;
+        type QObjectExternDisabled;
 
         #[qsignal]
         #[cfg(not(enabled))]
-        fn timeout(self: Pin<&mut QTimer>);
+        fn signal_disabled2(self: Pin<&mut QObjectExternDisabled>);
+
+        #[qsignal]
+        #[cfg(enabled)]
+        fn signal_enabled2(self: Pin<&mut QObjectExternDisabled>);
     }
 
+    // Enabled RustQt QObject
+    // - disabled and enabled qenum
+
+    #[qenum(QObjectEnabled)]
+    #[cfg(not(enabled))]
+    enum EnumDisabled1 {
+        A,
+    }
+
+    #[qenum(QObjectEnabled)]
+    #[cfg(enabled)]
+    enum EnumEnabled1 {
+        A,
+    }
+
+    // Enabled RustQt QObject
+    // - disabled and enabled inherit
+    // - disabled and enabled invokable
+    // - disabled and enabled signal
     unsafe extern "RustQt" {
-        // TODO: should we allow for disabling qobjects?
         #[qobject]
+        #[cfg(enabled)]
         // TODO: should we allow for disabling properties?
         // #[qproperty(i32, property_disabled, cfg(not(enabled)))]
-        type MyObject = super::MyObjectRust;
+        type QObjectEnabled = super::QObjectEnabledRust;
 
         #[inherit]
         #[cfg(not(enabled))]
-        fn inherit_disabled(self: &MyObject);
+        fn inherit_disabled(self: &QObjectEnabled);
+
+        #[inherit]
+        #[cfg(enabled)]
+        fn inherit_enabled(self: &QObjectEnabled);
 
         #[qinvokable]
         #[cfg(not(enabled))]
-        fn invokable_disabled(self: &MyObject);
+        fn invokable_disabled(self: &QObjectEnabled);
+
+        #[qinvokable]
+        #[cfg(enabled)]
+        fn invokable_enabled(self: &QObjectEnabled);
 
         #[qsignal]
         #[cfg(not(enabled))]
-        fn signal_disabled(self: Pin<&mut MyObject>);
+        fn signal_disabled(self: Pin<&mut QObjectEnabled>);
+
+        #[qsignal]
+        #[cfg(enabled)]
+        fn signal_enabled(self: Pin<&mut QObjectEnabled>);
+    }
+
+    // Dislabed RustQt QObject
+    // - disabled and enabled qenum
+
+    #[qenum(QObjectDisabled)]
+    #[cfg(not(enabled))]
+    enum EnumDisabled2 {
+        A,
+    }
+
+    #[qenum(QObjectDisabled)]
+    #[cfg(enabled)]
+    enum EnumEnabled2 {
+        A,
+    }
+
+    // Disabled RustQt QObject
+    // - disabled and enabled inherit
+    // - disabled and enabled invokable
+    // - disabled and enabled signal
+    unsafe extern "RustQt" {
+        #[qobject]
+        #[cfg(not(enabled))]
+        // TODO: should we allow for disabling properties?
+        // #[qproperty(i32, property_disabled, cfg(not(enabled)))]
+        type QObjectDisabled = super::QObjectDisabledRust;
+
+        #[inherit]
+        #[cfg(not(enabled))]
+        fn inherit_disabled(self: &QObjectDisabled);
+
+        #[inherit]
+        #[cfg(enabled)]
+        fn inherit_enabled(self: &QObjectDisabled);
+
+        #[qinvokable]
+        #[cfg(not(enabled))]
+        fn invokable_disabled(self: &QObjectDisabled);
+
+        #[qinvokable]
+        #[cfg(enabled)]
+        fn invokable_enabled(self: &QObjectDisabled);
+
+        #[qsignal]
+        #[cfg(not(enabled))]
+        fn signal_disabled(self: Pin<&mut QObjectDisabled>);
+
+        #[qsignal]
+        #[cfg(enabled)]
+        fn signal_enabled(self: Pin<&mut QObjectDisabled>);
     }
 }
--- crates/cxx-qt-gen/test_outputs/cfgs.cpp
+++ crates/cxx-qt-gen/test_outputs/cfgs.cpp
@@ -1,7 +1,179 @@
 #include "directory/file_ident.cxxqt.h"
 
-MyObject::MyObject(QObject* parent)
+// Define namespace otherwise we hit a GCC bug
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56480
+namespace rust::cxxqt1 {
+template<>
+SignalHandler<
+  ::rust::cxxqtgen1::QObjectExternEnabledCxxQtSignalParamssignal_enabled1*>::
+  ~SignalHandler() noexcept
+{
+  if (data[0] == nullptr && data[1] == nullptr) {
+    return;
+  }
+
+  drop_QObjectExternEnabled_signal_handler_signal_enabled1(::std::move(*this));
+}
+
+template<>
+template<>
+void
+SignalHandler<
+  ::rust::cxxqtgen1::QObjectExternEnabledCxxQtSignalParamssignal_enabled1*>::
+operator()<QObjectExternEnabled&>(QObjectExternEnabled& self)
+{
+  call_QObjectExternEnabled_signal_handler_signal_enabled1(*this, self);
+}
+
+static_assert(
+  alignof(
+    SignalHandler<::rust::cxxqtgen1::
+                    QObjectExternEnabledCxxQtSignalParamssignal_enabled1*>) <=
+    alignof(::std::size_t),
+  "unexpected aligment");
+static_assert(
+  sizeof(
+    SignalHandler<::rust::cxxqtgen1::
+                    QObjectExternEnabledCxxQtSignalParamssignal_enabled1*>) ==
+    sizeof(::std::size_t[2]),
+  "unexpected size");
+} // namespace rust::cxxqt1
+
+namespace rust::cxxqtgen1 {
+::QMetaObject::Connection
+QObjectExternEnabled_signal_enabled1Connect(
+  QObjectExternEnabled& self,
+  ::rust::cxxqtgen1::QObjectExternEnabledCxxQtSignalHandlersignal_enabled1
+    closure,
+  ::Qt::ConnectionType type)
+{
+  return ::QObject::connect(
+    &self,
+    &QObjectExternEnabled::signal_enabled1,
+    &self,
+    [&, closure = ::std::move(closure)]() mutable {
+      closure.template operator()<QObjectExternEnabled&>(self);
+    },
+    type);
+}
+} // namespace rust::cxxqtgen1
+
+// Define namespace otherwise we hit a GCC bug
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56480
+namespace rust::cxxqt1 {
+template<>
+SignalHandler<
+  ::rust::cxxqtgen1::QObjectExternDisabledCxxQtSignalParamssignal_enabled2*>::
+  ~SignalHandler() noexcept
+{
+  if (data[0] == nullptr && data[1] == nullptr) {
+    return;
+  }
+
+  drop_QObjectExternDisabled_signal_handler_signal_enabled2(::std::move(*this));
+}
+
+template<>
+template<>
+void
+SignalHandler<
+  ::rust::cxxqtgen1::QObjectExternDisabledCxxQtSignalParamssignal_enabled2*>::
+operator()<QObjectExternDisabled&>(QObjectExternDisabled& self)
+{
+  call_QObjectExternDisabled_signal_handler_signal_enabled2(*this, self);
+}
+
+static_assert(
+  alignof(
+    SignalHandler<::rust::cxxqtgen1::
+                    QObjectExternDisabledCxxQtSignalParamssignal_enabled2*>) <=
+    alignof(::std::size_t),
+  "unexpected aligment");
+static_assert(
+  sizeof(
+    SignalHandler<::rust::cxxqtgen1::
+                    QObjectExternDisabledCxxQtSignalParamssignal_enabled2*>) ==
+    sizeof(::std::size_t[2]),
+  "unexpected size");
+} // namespace rust::cxxqt1
+
+namespace rust::cxxqtgen1 {
+::QMetaObject::Connection
+QObjectExternDisabled_signal_enabled2Connect(
+  QObjectExternDisabled& self,
+  ::rust::cxxqtgen1::QObjectExternDisabledCxxQtSignalHandlersignal_enabled2
+    closure,
+  ::Qt::ConnectionType type)
+{
+  return ::QObject::connect(
+    &self,
+    &QObjectExternDisabled::signal_enabled2,
+    &self,
+    [&, closure = ::std::move(closure)]() mutable {
+      closure.template operator()<QObjectExternDisabled&>(self);
+    },
+    type);
+}
+} // namespace rust::cxxqtgen1
+
+// Define namespace otherwise we hit a GCC bug
+// https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56480
+namespace rust::cxxqt1 {
+template<>
+SignalHandler<
+  ::rust::cxxqtgen1::QObjectEnabledCxxQtSignalParamssignal_enabled*>::
+  ~SignalHandler() noexcept
+{
+  if (data[0] == nullptr && data[1] == nullptr) {
+    return;
+  }
+
+  drop_QObjectEnabled_signal_handler_signal_enabled(::std::move(*this));
+}
+
+template<>
+template<>
+void
+SignalHandler<
+  ::rust::cxxqtgen1::QObjectEnabledCxxQtSignalParamssignal_enabled*>::
+operator()<QObjectEnabled&>(QObjectEnabled& self)
+{
+  call_QObjectEnabled_signal_handler_signal_enabled(*this, self);
+}
+
+static_assert(
+  alignof(SignalHandler<
+          ::rust::cxxqtgen1::QObjectEnabledCxxQtSignalParamssignal_enabled*>) <=
+    alignof(::std::size_t),
+  "unexpected aligment");
+static_assert(
+  sizeof(SignalHandler<
+         ::rust::cxxqtgen1::QObjectEnabledCxxQtSignalParamssignal_enabled*>) ==
+    sizeof(::std::size_t[2]),
+  "unexpected size");
+} // namespace rust::cxxqt1
+
+namespace rust::cxxqtgen1 {
+::QMetaObject::Connection
+QObjectEnabled_signal_enabledConnect(
+  QObjectEnabled& self,
+  ::rust::cxxqtgen1::QObjectEnabledCxxQtSignalHandlersignal_enabled closure,
+  ::Qt::ConnectionType type)
+{
+  return ::QObject::connect(
+    &self,
+    &QObjectEnabled::signal_enabled,
+    &self,
+    [&, closure = ::std::move(closure)]() mutable {
+      closure.template operator()<QObjectEnabled&>(self);
+    },
+    type);
+}
+} // namespace rust::cxxqtgen1
+
+QObjectEnabled::QObjectEnabled(QObject* parent)
   : QObject(parent)
-  , ::rust::cxxqt1::CxxQtType<MyObjectRust>(::cxx_qt_MyObject::createRs())
+  , ::rust::cxxqt1::CxxQtType<QObjectEnabledRust>(
+      ::cxx_qt_QObjectEnabled::createRs())
 {
 }
