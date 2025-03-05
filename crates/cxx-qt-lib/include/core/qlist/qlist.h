524a7908562abf72b842365e55696f2c60d11152
Quentin Weber <quentin.weber@kdab.com>
2025-03-05 14:14:51 +0100

    remove code duplication

---
+++ crates/cxx-qt-lib/include/core/qlist/qlist.h
@@ -0,0 +1,43 @@
+// clang-format off
+// SPDX-FileCopyrightText: 2022 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// clang-format on
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+#pragma once
+
+#include <cstdint>
+
+#include "qlist_QByteArray.h"
+#include "qlist_bool.h"
+#include "qlist_f32.h"
+#include "qlist_f64.h"
+#include "qlist_i16.h"
+#include "qlist_i32.h"
+#include "qlist_i64.h"
+#include "qlist_i8.h"
+#ifdef CXX_QT_GUI_FEATURE
+#include "qlist_QColor.h"
+#endif
+#include "qlist_QDate.h"
+#include "qlist_QDateTime.h"
+#include "qlist_QLine.h"
+#include "qlist_QLineF.h"
+#include "qlist_QMargins.h"
+#include "qlist_QMarginsF.h"
+#include "qlist_QPersistentModelIndex.h"
+#include "qlist_QPoint.h"
+#include "qlist_QPointF.h"
+#include "qlist_QRect.h"
+#include "qlist_QRectF.h"
+#include "qlist_QSize.h"
+#include "qlist_QSizeF.h"
+#include "qlist_QString.h"
+#include "qlist_QTime.h"
+#include "qlist_QUrl.h"
+#include "qlist_QUuid.h"
+#include "qlist_QVariant.h"
+#include "qlist_u16.h"
+#include "qlist_u32.h"
+#include "qlist_u64.h"
+#include "qlist_u8.h"
