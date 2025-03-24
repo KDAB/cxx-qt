39a4dc2c7312097bb25c9deb2d3302c424c19930
whs31 <riazantsev.dl@edu.spbstu.ru>
2025-03-24 20:31:37 +0300

    Fixed broken link to book in Rust generated code

--- crates/cxx-qt-gen/src/generator/rust/qobject.rs
+++ crates/cxx-qt-gen/src/generator/rust/qobject.rs
@@ -151,7 +151,7 @@ fn generate_qobject_definitions(
                     #[doc = "\n"]
                     #[doc = "Use this type when referring to the QObject as a pointer"]
                     #[doc = "\n"]
-                    #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/qobject/generated-qobject.html>"]
+                    #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/concepts/generated_qobject.html>"]
                     #namespace
                     #cxx_name
                     type #cpp_class_name_rust;
