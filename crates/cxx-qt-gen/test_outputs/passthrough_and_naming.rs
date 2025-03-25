f80b7718373e5c0257e9f2e0935228bd395e3454
Leon Matthes <leon.matthes@kdab.com>
2025-03-25 09:13:41 +0100

    Update remaining links to QObject book page
    Thanks to @whs31 for spotting the issue

--- crates/cxx-qt-gen/test_outputs/passthrough_and_naming.rs
+++ crates/cxx-qt-gen/test_outputs/passthrough_and_naming.rs
@@ -78,7 +78,7 @@ pub mod ffi {
         #[doc = "\n"]
         #[doc = "Use this type when referring to the QObject as a pointer"]
         #[doc = "\n"]
-        #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/qobject/generated-qobject.html>"]
+        #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/concepts/generated_qobject.html>"]
         #[namespace = "cxx_qt::multi_object"]
         type MyObject;
     }
@@ -188,7 +188,7 @@ pub mod ffi {
         #[doc = "\n"]
         #[doc = "Use this type when referring to the QObject as a pointer"]
         #[doc = "\n"]
-        #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/qobject/generated-qobject.html>"]
+        #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/concepts/generated_qobject.html>"]
         #[namespace = "second_object"]
         type SecondObject;
     }
@@ -306,7 +306,7 @@ pub mod ffi {
         #[doc = "\n"]
         #[doc = "Use this type when referring to the QObject as a pointer"]
         #[doc = "\n"]
-        #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/qobject/generated-qobject.html>"]
+        #[doc = "See the book for more information: <https://kdab.github.io/cxx-qt/book/concepts/generated_qobject.html>"]
         #[namespace = "my_namespace"]
         #[doc = "\n\nNote: The C++ name of this QObject is: "]
         #[doc = "MyCxxName"]
