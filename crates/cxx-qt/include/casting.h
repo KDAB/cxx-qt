fdd65e94eff925d796922aa36998c00bd7a409ae
Leon Matthes <leon.matthes@kdab.com>
2025-03-31 11:37:53 +0200

    Improve downcast C++ implementations
    Add detection whether to use qobject_cast or dynamic_cast via template
    specialization.
    
    Also add additional checks that a static_cast downcast is sane to do
    (i.e. at least the size of the types must be the same).

--- crates/cxx-qt/include/casting.h
+++ crates/cxx-qt/include/casting.h
@@ -18,7 +18,24 @@ upcastPtr(const Sub* sub)
   return static_cast<const Base*>(sub);
 }
 
-template<typename Sub, typename Base>
+// Main downcasting function, for non-QObject types
+template<typename Sub,
+         typename Base,
+         std::enable_if_t<!std::is_base_of_v<QObject, Sub>, bool> = true>
+const Sub*
+downcastPtr(const Base* base)
+{
+  static_assert(std::is_base_of_v<Base, Sub>);
+  static_assert(std::is_polymorphic_v<Sub>,
+                "Downcasting requires a polymorphic type (e.g. a type with at "
+                "least one virtual method)!");
+  return dynamic_cast<const Sub*>(base);
+}
+
+// Downcasting function for QObject types, enabled via SFINAE
+template<typename Sub,
+         typename Base,
+         std::enable_if_t<std::is_base_of_v<QObject, Sub>, bool> = true>
 const Sub*
 downcastPtr(const Base* base)
 {
@@ -26,11 +43,18 @@ downcastPtr(const Base* base)
   return qobject_cast<const Sub*>(base);
 }
 
+// Warning: This function is highly unsafe, use with caution!
+// It is only safe to use if you are 100% sure that the Sub and Base types are
+// of the same size and that all Base instances are also valid Sub instances!
+//
+// This is mostly the case for types like QStringList, that are just an
+// extension of their parent types and don't add any data members.
 template<typename Sub, typename Base>
 const Sub*
 downcastPtrStatic(const Base* base)
 {
   static_assert(std::is_base_of_v<Base, Sub>);
+  static_assert(sizeof(Base) == sizeof(Sub));
   return static_cast<const Sub*>(base);
 }
 
