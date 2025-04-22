c60bf6e9f0b1c2ff0c5336bf8384d68b2b505403
Andrew Hayzen <andrew.hayzen@kdab.com>
2025-04-22 14:55:23 +0100

    qt-build-utils: add a QtTool enum for later

---
+++ crates/qt-build-utils/src/tool.rs
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Andrew Hayzen <andrew.hayzen@kdab.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+/// An enum representing known Qt tools
+#[non_exhaustive]
+pub enum QtTool {
+    /// Moc
+    Moc,
+    /// Rcc (Qt resources)
+    Rcc,
+    /// Qml cachegen
+    QmlCacheGen,
+    /// Qml Type Registrar
+    QmlTypeRegistrar,
+    // TODO: could add a Custom(&str) thing here
+}
+
+impl QtTool {
+    pub(crate) fn binary_name(&self) -> &str {
+        match self {
+            Self::Moc => "moc",
+            Self::Rcc => "rcc",
+            Self::QmlCacheGen => "qmlcachegen",
+            Self::QmlTypeRegistrar => "qmltyperegistrar",
+        }
+    }
+}
