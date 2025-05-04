52f36a3676e27485093db86b637208a208e43af6
Joshua Booth <joshua.n.booth@gmail.com>
2025-05-04 14:51:25 -0700

    cxx-qt-lib-extras: export QEventLoop enums

--- crates/cxx-qt-lib-extras/src/core/mod.rs
+++ crates/cxx-qt-lib-extras/src/core/mod.rs
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 mod qeventloop;
-pub use qeventloop::QEventLoop;
+pub use qeventloop::{QEventLoop, QEventLoopProcessEventsFlag, QEventLoopProcessEventsFlags};
 
 mod qelapsedtimer;
 pub use qelapsedtimer::QElapsedTimer;
