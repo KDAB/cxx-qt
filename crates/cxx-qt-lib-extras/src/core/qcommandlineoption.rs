53a46fb96ba565fffe119ef317488a02c5dfe678
Joshua Booth <joshua.n.booth@gmail.com>
2025-06-17 17:23:24 -0700

    cxx-qt-lib-extras: import documentation from Qt

--- crates/cxx-qt-lib-extras/src/core/qcommandlineoption.rs
+++ crates/cxx-qt-lib-extras/src/core/qcommandlineoption.rs
@@ -4,6 +4,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use cxx::{type_id, ExternType};
+use cxx_qt_lib::{QString, QStringList};
 use std::mem::MaybeUninit;
 
 #[cxx::bridge]
@@ -26,22 +27,36 @@ mod ffi {
         /// Returns the names set for this option.
         fn names(self: &QCommandLineOption) -> QStringList;
 
-        /// Sets the default value used for this option to defaultValue.
+        /// Sets the default value used for this option to `default_value`.
+        ///
+        /// The default value is used if the user of the application does not specify the option on the command line.
+        ///
+        /// If `default_value` is empty, the option has no default values.
         #[rust_name = "set_default_value"]
-        fn setDefaultValue(self: &mut QCommandLineOption, value: &QString);
+        fn setDefaultValue(self: &mut QCommandLineOption, default_value: &QString);
 
-        /// Sets the list of default values used for this option to defaultValues.
+        /// Sets the list of default values used for this option to `default_values`.
+        ///
+        /// The default values are used if the user of the application does not specify the option on the command line.
         #[rust_name = "set_default_values"]
-        fn setDefaultValues(self: &mut QCommandLineOption, values: &QStringList);
+        fn setDefaultValues(self: &mut QCommandLineOption, default_values: &QStringList);
 
-        /// Sets the description used for this option to description.
+        /// Sets the description used for this option to `description`.
         /// It is customary to add a "." at the end of the description.
+        ///
+        /// The description is used by [QCommandLineParser::showHelp](https://doc.qt.io/qt/qcommandlineparser.html#showHelp)().
         #[rust_name = "set_description"]
         fn setDescription(self: &mut QCommandLineOption, description: &QString);
 
-        /// Sets the name of the expected value, for the documentation, to valueName.
+        /// Sets the name of the expected value, for the documentation, to `value_name`.
+        ///
+        /// Options without a value assigned have a boolean-like behavior: either the user specifies –option or they don't.
+        ///
+        /// Options with a value assigned need to set a name for the expected value, for the documentation of the option in the help output. An option with names `o` and `output`, and a value name of file will appear as `-o, --output <file>`.
+        ///
+        /// Call [`QCommandLineParser::value`](crate::QCommandLineParser::value) if you expect the option to be present only once, and [`QCommandLineParser::values`](crate::QCommandLineParser::values) if you expect that option to be present multiple times.
         #[rust_name = "set_value_name"]
-        fn setValueName(self: &mut QCommandLineOption, valueName: &QString);
+        fn setValueName(self: &mut QCommandLineOption, value_name: &QString);
 
         /// Returns the name of the expected value.
         #[rust_name = "value_name"]
@@ -70,35 +85,38 @@ mod ffi {
     }
 }
 
+/// The `QCommandLineOption` class defines a possible command-line option.
+///
+/// Qt Documentation: [QCommandLineOption](https://doc.qt.io/qt/qcommandlineoption.html#details)
 #[repr(C)]
 pub struct QCommandLineOption {
     _space: MaybeUninit<usize>,
 }
 
 impl Clone for QCommandLineOption {
-    /// Constructs a copy of other.
+    /// Constructs a copy of this `QCommandLineOption`.
     fn clone(&self) -> Self {
         ffi::qcommandlineoption_init_from_qcommandlineoption(self)
     }
 }
 
 impl Drop for QCommandLineOption {
-    /// Destroys the qcommandlineoption.
+    /// Destroys the `QCommandLineOption`.
     fn drop(&mut self) {
         ffi::qcommandlineoption_drop(self)
     }
 }
 
-impl From<&ffi::QString> for QCommandLineOption {
+impl From<&QString> for QCommandLineOption {
     /// Constructs a command line option object with the name name.
-    fn from(name: &ffi::QString) -> Self {
+    fn from(name: &QString) -> Self {
         ffi::qcommandlineoption_init_from_qstring(name)
     }
 }
 
-impl From<&ffi::QStringList> for QCommandLineOption {
+impl From<&QStringList> for QCommandLineOption {
     /// Constructs a command line option object with the name name.
-    fn from(names: &ffi::QStringList) -> Self {
+    fn from(names: &QStringList) -> Self {
         ffi::qcommandlineoption_init_from_qstringlist(names)
     }
 }
