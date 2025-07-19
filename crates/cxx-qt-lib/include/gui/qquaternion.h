7f96ef1232bae7bb7ac693bce790ed1713769863
Joshua Booth <joshua.n.booth@gmail.com>
2025-07-19 09:04:57 -0700

    cxx-qt-lib: fix SPDX dates

--- crates/cxx-qt-lib/include/gui/qquaternion.h
+++ crates/cxx-qt-lib/include/gui/qquaternion.h
@@ -1,5 +1,5 @@
 // clang-format off
-// SPDX-FileCopyrightText: 2023 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 // clang-format on
 // SPDX-FileContributor: Joshua Booth <joshua.n.booth@gmail.com>
 //
