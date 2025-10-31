9af901d06f1e080204b6f1f12b0f69870ec5b55d
Leon Matthes <leon.matthes@kdab.com>
2025-10-31 14:49:20 +0100

    Update build.rs after merge

--- examples/span-inspector/build.rs
+++ examples/span-inspector/build.rs
@@ -6,13 +6,8 @@
 use cxx_qt_build::{CxxQtBuilder, QmlModule};
 
 fn main() {
-    let qml_module: QmlModule<'static, &str, &str> = QmlModule {
-        uri: "com.kdab.cxx_qt.span_inspector",
-        rust_files: &["src/inspector.rs"],
-        qml_files: &["qml/main.qml"],
-        ..Default::default()
-    };
-    CxxQtBuilder::new()
+    let qml_module = QmlModule::new("com.kdab.cxx_qt.span_inspector").qml_file("qml/main.qml");
+    CxxQtBuilder::new_qml_module(qml_module)
         // Link Qt's Network library
         // - Qt Core is always linked
         // - Qt Gui is linked by enabling the qt_gui Cargo feature of cxx-qt-lib.
@@ -20,6 +15,6 @@ fn main() {
         // - Qt Qml requires linking Qt Network on macOS
         .qt_module("Network")
         .qt_module("Quick")
-        .qml_module(qml_module)
+        .file("src/inspector.rs")
         .build();
 }
