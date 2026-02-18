44c7cd6c4f2fe2fddab9d2e16984c6a7576b64bd
Andrew Hayzen <95233486+ahayzen-kdab@users.noreply.github.com>
2026-02-19 02:46:01 +0900

    Bump cxx and rustc versions (#1416)
    * deps: bump cxx from 1.0.144 to 1.0.176
    
    * rustc: bump MSRV to 1.85.0 for rust-edition 2024
    
    This is also the same version as the latest CXX package.
    
    * clippy: fix new lint warnings for 1.85
    
    ---------
    
    Co-authored-by: Joshua Booth <joshua.n.booth@gmail.com>

--- scripts/grcov_cxx_qt.sh
+++ scripts/grcov_cxx_qt.sh
@@ -23,8 +23,8 @@ rm -f "$SCRIPTPATH"/coverage/*.profraw
 # We can use rustup component add llvm-tools but this can be out of sync
 # See versions from the table in this link
 # https://github.com/taiki-e/cargo-llvm-cov?tab=readme-ov-file#get-coverage-of-cc-code-linked-to-rust-librarybinary
-if [ ! -d /usr/lib/llvm-17/bin/ ]; then
-  echo "LLVM 17 not found"
+if [ ! -d /usr/lib/llvm-19/bin/ ]; then
+  echo "LLVM 19 not found"
 fi
 
 export CARGO_INCREMENTAL=0
@@ -32,7 +32,7 @@ export RUSTFLAGS="-Cinstrument-coverage"
 export LLVM_PROFILE_FILE="$SCRIPTPATH/coverage/coverage_data-%p-%m.profraw"
 cargo build --package cxx-qt-gen
 cargo test --package cxx-qt-gen
-# Note that --llvm-path is important here to ensure the matching llvm version to the Rust version (1.77.x)
+# Note that --llvm-path is important here to ensure the matching llvm version to the Rust version (1.85.x)
 # Note that --keep-only is important here to ensure crates.io paths don't conflict
-grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --llvm --llvm-path=/usr/lib/llvm-17/bin/ --keep-only "crates/*" -o ./target/debug/ --excl-start CODECOV_EXCLUDE_START --excl-stop CODECOV_EXCLUDE_STOP
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing --llvm --llvm-path=/usr/lib/llvm-19/bin/ --keep-only "crates/*" -o ./target/debug/ --excl-start CODECOV_EXCLUDE_START --excl-stop CODECOV_EXCLUDE_STOP
 echo "Coverage html report generated in $(realpath "$SCRIPTPATH"/../target/debug/html)"
