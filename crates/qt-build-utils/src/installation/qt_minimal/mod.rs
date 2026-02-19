b589b025222cd9e7933f6c880dfd124123d5f2a5
Andrew Hayzen <andrew.hayzen@kdab.com>
2026-02-19 16:59:17 +0000

    qt_minimal: add basic implementation that downloads artifacts to cwd

--- crates/qt-build-utils/src/installation/qt_minimal/mod.rs
+++ crates/qt-build-utils/src/installation/qt_minimal/mod.rs
@@ -3,38 +3,103 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-#[allow(dead_code)]
 mod artifact;
-#[allow(dead_code)]
 mod checksum;
-#[allow(dead_code)]
 mod download;
-#[allow(dead_code)]
 mod extract;
+
+use std::path::PathBuf;
 
 use crate::QtInstallation;
 
 /// A implementation of [QtInstallation] using qtminimal
-pub struct QtInstallationQtMinimal;
+pub struct QtInstallationQtMinimal {
+    path_qt: PathBuf,
+    version: semver::Version,
+}
+
+impl TryFrom<semver::Version> for QtInstallationQtMinimal {
+    type Error = anyhow::Error;
+
+    fn try_from(version: semver::Version) -> Result<Self, Self::Error> {
+        // Parse all artifacts
+        let manifest: artifact::ParsedQtManifest =
+            serde_json::from_str(qt_artifacts::QT_MANIFEST_JSON)?;
+
+        // Find artifacts matching Qt version
+        let artifacts: Vec<artifact::ParsedQtArtifact> = manifest
+            .artifacts
+            .into_iter()
+            // TODO: check OS and arch and likely make a function on the manifest struct
+            .filter(|artifact| artifact.version == version)
+            .collect();
+
+        // Find the first bin / include
+        let artifact_bin = artifacts
+            .iter()
+            .find(|artifact| artifact.content.contains(&"bin".to_string()))
+            .expect("At least one artifact to have a bin folder");
+        let artifact_include = artifacts
+            .iter()
+            .find(|artifact| artifact.content.contains(&"include".to_string()))
+            .expect("At least one artifact to have an include folder");
+
+        // Download the artifacts
+        let extract_target_dir = PathBuf::from(".").canonicalize()?;
+        artifact_bin.download_and_extract(&extract_target_dir)?;
+        if artifact_bin != artifact_include {
+            artifact_include.download_and_extract(&extract_target_dir)?;
+        }
+
+        Ok(Self {
+            path_qt: extract_target_dir.join("qt"),
+            version,
+        })
+    }
+}
 
 impl QtInstallation for QtInstallationQtMinimal {
     fn framework_paths(&self, _qt_modules: &[String]) -> Vec<std::path::PathBuf> {
-        todo!()
+        // TODO: macos support
+        vec![]
     }
 
     fn include_paths(&self, _qt_modules: &[String]) -> Vec<std::path::PathBuf> {
-        todo!()
+        let mut paths = vec![];
+        let root_path = self.path_qt.join("include");
+        paths.push(root_path);
+
+        // TODO: loop over qt modules
+
+        paths
     }
 
     fn link_modules(&self, _builder: &mut cc::Build, _qt_modules: &[String]) {
-        todo!()
+        let lib_path = self.path_qt.join("lib");
+        println!("cargo::rustc-link-search={}", lib_path.display());
+
+        // TODO: loop over qt modules
     }
 
-    fn try_find_tool(&self, _tool: crate::QtTool) -> anyhow::Result<std::path::PathBuf> {
-        todo!()
+    fn try_find_tool(&self, tool: crate::QtTool) -> anyhow::Result<std::path::PathBuf> {
+        // Tools could be either in libexec or bin
+        let path_bin = self.path_qt.join("bin").join(tool.binary_name());
+        let path_libexec = self.path_qt.join("libexec").join(tool.binary_name());
+
+        if path_bin.exists() {
+            return Ok(path_bin);
+        } else if path_libexec.exists() {
+            return Ok(path_libexec);
+        }
+
+        Err(anyhow::anyhow!(
+            "Failed to find {} in bin/ or libexec/ under {}",
+            tool.binary_name(),
+            self.path_qt.display()
+        ))
     }
 
     fn version(&self) -> semver::Version {
-        todo!()
+        self.version.clone()
     }
 }
