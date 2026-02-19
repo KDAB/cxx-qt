2a14e1595bc079a8db4f7eb222175809183e8a60
Andrew Hayzen <andrew.hayzen@kdab.com>
2026-02-19 16:59:17 +0000

    qt_minimal: return the downloaded path or an error

--- crates/qt-build-utils/src/installation/qt_minimal/download.rs
+++ crates/qt-build-utils/src/installation/qt_minimal/download.rs
@@ -4,26 +4,28 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use reqwest::blocking::Client;
-use std::io::{BufWriter, Write};
+use std::{
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
 use tempfile::TempDir;
 
 /// Download a file into a temporary directory over HTTP.
 /// Currently, has no error handling, and will crash on any errors.
 /// On my machine, runs about twice as long as wget (34 seconds compared to 17)
-pub(crate) fn download_from_url(url: &str, filename: &str, temp_dir: &TempDir, client: &Client) {
-    if let Ok(response) = client.get(url).send() {
-        let download_path = temp_dir.path().join(filename);
-        println!("temp path: {:?}", download_path);
-        let file = std::fs::File::create(&download_path).expect("Failed to create file");
-        let mut writer = BufWriter::new(file);
-        let content = response.bytes().expect("Failed to read content");
+pub(crate) fn download_from_url(
+    url: &str,
+    filename: &str,
+    temp_dir: &TempDir,
+    client: &Client,
+) -> anyhow::Result<PathBuf> {
+    let response = client.get(url).send()?;
+    let download_path = temp_dir.path().join(filename);
+    let file = std::fs::File::create(&download_path)?;
+    let mut writer = BufWriter::new(file);
+    let content = response.bytes()?;
 
-        writer
-            .write_all(&content)
-            .expect("Failed to write data to file");
+    writer.write_all(&content)?;
 
-        println!("Saved image to {}", download_path.to_str().unwrap());
-    } else {
-        panic!("Failed to download file");
-    }
+    Ok(download_path)
 }
