a6a1444b6154b34a9d26c9a13318575821f64310
Joshua Booth <joshua.n.booth@gmail.com>
2026-02-22 19:29:24 -0800

    cxx-qt-gen: remove unnecessary #[allow(private_interfaces)]`

--- crates/cxx-qt-gen/src/generator/rust/qenum.rs
+++ crates/cxx-qt-gen/src/generator/rust/qenum.rs
@@ -68,7 +68,6 @@ fn generate_member_qenum(qenum: &ParsedQEnum) -> GeneratedRustFragment {
             item.span() =>
             #(#cfgs)*
             extern "C++" {
-                #[allow(private_interfaces)]
                 #(#docs)*
                 #(#qenum_attrs)*
                 type #qenum_ident = super::#module_name::#qenum_ident;
@@ -219,7 +218,6 @@ mod tests {
             &generated.cxx_mod_contents[0],
             quote! {
                 extern "C++" {
-                    #[allow(private_interfaces)]
                     #[doc = r" Doc comment"]
                     #[namespace = "my_namespace"]
                     type MyEnum = super::cxx_qt_private_qenum_MyEnum::MyEnum;
