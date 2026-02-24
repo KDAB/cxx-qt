8a9b79ee9de6d12d8e1e36493eb2ef1925cf5802
Ben Ford <benjaminford2005@gmail.com>
2026-02-24 14:29:07 +0000

    Add directory walk to find qt artifacts

--- crates/qt-build-utils/src/installation/qt_minimal/artifact.rs
+++ crates/qt-build-utils/src/installation/qt_minimal/artifact.rs
@@ -3,9 +3,9 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use std::path::{Path, PathBuf};
-
+use semver::Version;
 use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Serialize, Deserialize)]
 /// A Qt manifest.json file, which specifies a set of artifacts needed for installation
@@ -17,7 +17,7 @@ pub(crate) struct ParsedQtManifest {
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 /// Descriptor for a Qt artifact, included download information
 pub(crate) struct ParsedQtArtifact {
-    pub(crate) version: semver::Version,
+    pub(crate) version: Version,
     pub(crate) arch: String,
     pub(crate) os: String,
     pub(crate) url: String,
@@ -29,7 +29,10 @@ impl ParsedQtArtifact {
     /// Download the artifact and extract to the given target path
     pub fn download_and_extract(&self, target_path: &Path) -> PathBuf {
         // Download to a temporary location
-        let http_client = reqwest::blocking::Client::new();
+        let http_client = reqwest::blocking::Client::builder()
+            .timeout(None)
+            .build()
+            .expect("Http client failed to build");
         let temp_dir = tempfile::TempDir::new().expect("Could not create temporary directory");
         let archive_path =
             super::download::download_from_url(&self.url, &self.sha256, &temp_dir, &http_client)
@@ -44,6 +47,24 @@ impl ParsedQtArtifact {
             .expect("Could not extract archive into target");
 
         target_path.to_path_buf()
+    }
+
+    /// Used to create a found artifact without a checksum, used by local artifact discovery
+    pub fn new(
+        version: Version,
+        arch: String,
+        os: String,
+        url: String,
+        content_type: String,
+    ) -> Self {
+        Self {
+            version,
+            arch,
+            os,
+            url,
+            sha256: "".to_string(),
+            content: vec![content_type],
+        }
     }
 
     /// Assert that the hashes are the same, from bytes
