9fd213f925ba161c82944faee56c4ebc9f3b878f
Joshua Booth <joshua.n.booth@gmail.com>
2026-05-07 15:29:36 -0700

    cxx-qt-lib-extras: add license header to util.rs

--- crates/cxx-qt-lib-extras/src/util.rs
+++ crates/cxx-qt-lib-extras/src/util.rs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2026 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+// SPDX-FileContributor: Joshua Booth <joshua.n.booth@gmail.com>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
 use std::mem::MaybeUninit;
 
 /// # Safety
